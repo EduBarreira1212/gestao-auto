f27e0adbfad0910f809f07d39ef47b8987d193c8
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-27 12:49:16 -0300

    feat: add validations and encryptations

--- server/src/use-cases/create-user.js
+++ server/src/use-cases/create-user.js
@@ -1,10 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 import { PostgresCreateUserRepositorie } from '../respositories/postgres/create-user';
+import { PostgresGetUserByEmailRepositorie } from '../respositories/postgres/get-user-by-email';
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
+        const postgresGetUserByEmailRepositorie =
+            new PostgresGetUserByEmailRepositorie();
+
+        const emailExists = await postgresGetUserByEmailRepositorie.execute(
+            createUserParams.email
+        );
+
+        if (emailExists) {
+            throw new Error('Email already exists');
+        }
+
+        const userId = uuidv4();
+
+        const encryptedPassword = bcrypt.hashSync(createUserParams.password, 10);
+
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: encryptedPassword,
+        };
+
         const postgresCreateUserRepositorie = new PostgresCreateUserRepositorie();
-        const userCreated =
-            await postgresCreateUserRepositorie.execute(createUserParams);
+
+        const userCreated = await postgresCreateUserRepositorie.execute(user);
+
         return userCreated;
     }
 }
