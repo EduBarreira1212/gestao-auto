c824991c1e442168b519dcf792daa8f45266b424
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-28 11:58:16 -0300

    feat: add field and password validation when creating user

--- server/src/controllers/create-user.js
+++ server/src/controllers/create-user.js
@@ -1,14 +1,35 @@
-import { CreateUserUseCase } from '../use-cases/create-user';
+import { CreateUserUseCase } from '../use-cases/create-user.js';
 
 export class CreateUserController {
     async execute(httpRequest) {
         try {
             const params = httpRequest.body;
+
+            const requiredFields = ['name', 'email', 'password'];
+
+            for (const field of requiredFields) {
+                if (!params[field] || params[field].trim().length === 0) {
+                    return {
+                        statusCode: 404,
+                        body: { message: `Missing ${field}` },
+                    };
+                }
+            }
+
+            const isPasswordValid = params.password.length >= 5;
+
+            if (!isPasswordValid) {
+                return { statusCode: 404, body: { message: 'Password invalid' } };
+            }
+
             const createUserUseCase = new CreateUserUseCase();
+
             const user = await createUserUseCase.execute(params);
+
             return { statusCode: 201, body: user };
         } catch (error) {
             console.log(error);
+            return { statusCode: 404, body: error };
         }
     }
 }
