35386b58afd55303f7493f66e54c52b4ea3bc01f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-28 11:57:49 -0300

    feat: add get user by id route

--- server/src/index.js
+++ server/src/index.js
@@ -4,15 +4,24 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
 import { CreateUserController } from './controllers/create-user.js';
+import { GetUserByIdCrontroller } from './controllers/get-user-by-id.js';
 
 const app = express();
 
 app.use(express.json());
 
+app.get('/api/get-user/:userId', async (req, res) => {
+    const getUserById = new GetUserByIdCrontroller();
+
+    const { statusCode, body } = await getUserById.execute(req);
+
+    res.status(statusCode).send(body);
+});
+
 app.post('/api/create-user', async (req, res) => {
     const createUserController = new CreateUserController();
 
-    const { statusCode, body } = createUserController.execute(req);
+    const { statusCode, body } = await createUserController.execute(req);
 
     res.status(statusCode).send(body);
 });
