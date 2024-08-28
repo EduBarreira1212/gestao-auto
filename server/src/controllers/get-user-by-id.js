421c7053d97fd64acfd2f592ab6d83413db2eaae
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-28 11:58:45 -0300

    fix: add a try catch

--- server/src/controllers/get-user-by-id.js
+++ server/src/controllers/get-user-by-id.js
@@ -1,22 +1,27 @@
 import { validate } from 'uuid';
-import { GetUserById } from '../use-cases/get-user-by-id';
+import { GetUserById } from '../use-cases/get-user-by-id.js';
 
 export class GetUserByIdCrontroller {
     async execute(httpRequest) {
-        const isIdValid = validate(httpRequest.params.userId);
+        try {
+            const isIdValid = validate(httpRequest.params.userId);
 
-        if (!isIdValid) {
-            return { status: 404, message: 'ID invalid' };
+            if (!isIdValid) {
+                return { statusCode: 404, body: { message: 'ID invalid' } };
+            }
+
+            const getUserById = new GetUserById();
+
+            const user = await getUserById.execute(httpRequest.params.userId);
+
+            if (!user) {
+                return { statusCode: 404, body: { message: 'User not found' } };
+            }
+
+            return { statusCode: 200, body: user };
+        } catch (error) {
+            console.log(error);
+            return { statusCode: 500, body: { message: 'Server error' } };
         }
-
-        const getUserById = new GetUserById();
-
-        const user = getUserById.execute(httpRequest.params.userIds);
-
-        if (!user) {
-            return { status: 404, message: 'User not found' };
-        }
-
-        return user;
     }
 }
