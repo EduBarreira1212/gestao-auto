a137e0db8e9e33c787ce057ed1cd31323ca5ee89
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-29 11:41:15 -0300

    feat: creates update and delete user routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env' });
-
 import { CreateUserController } from './controllers/create-user.js';
 import { GetUserByIdCrontroller } from './controllers/get-user-by-id.js';
+import { UpdateUserController } from './controllers/update-user.js';
+import { DeleteUserController } from './controllers/delete-user.js';
+
+dotenv.config({ path: '.env' });
 
 const app = express();
 
@@ -22,6 +24,22 @@ app.post('/api/create-user', async (req, res) => {
     const createUserController = new CreateUserController();
 
     const { statusCode, body } = await createUserController.execute(req);
+
+    res.status(statusCode).send(body);
+});
+
+app.patch('/api/update-user/:userId', async (req, res) => {
+    const updateUserController = new UpdateUserController();
+
+    const { statusCode, body } = await updateUserController.execute(req);
+
+    res.status(statusCode).send(body);
+});
+
+app.delete('/api/delete-user/:userId', async (req, res) => {
+    const deleteUserController = new DeleteUserController();
+
+    const { statusCode, body } = await deleteUserController.execute(req);
 
     res.status(statusCode).send(body);
 });
