1aa1691cb6f14ceb2b1060aec86a4dfeb50104c6
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-02 11:14:58 -0300

    feat: creates a get cars by user id controller

--- server/src/factories/controllers/car.js
+++ server/src/factories/controllers/car.js
@@ -1,10 +1,13 @@
 import { CreateCarController } from '../../controllers/car/create-car.js';
 import { GetCarByIdController } from '../../controllers/car/get-car-by-id.js';
+import { GetCarsByUserIdController } from '../../controllers/car/get-cars-by-user-id.js';
 import { PostgresCreateCarRepository } from '../../respositories/postgres/car/create-car.js';
 import { PostgresGetCarByIdRepository } from '../../respositories/postgres/car/get-car-by-id.js';
+import { PostgresGetCarsByUserIdRepository } from '../../respositories/postgres/car/get-cars-by-user-id.js';
 import { PostgresGetUserById } from '../../respositories/postgres/user/get-user-by-id.js';
 import { CreateCarUseCase } from '../../use-cases/car/create-car.js';
 import { GetCarByIdUseCase } from '../../use-cases/car/get-car-by-id.js';
+import { GetCarsByUserIdUseCase } from '../../use-cases/car/get-cars-by-user-id.js';
 
 export const makeCreateCarController = () => {
     const postgresGetUserByIdRepository = new PostgresGetUserById();
@@ -28,4 +31,21 @@ export const makeGetCarByIdController = () => {
     const getCarByIdController = new GetCarByIdController(getCarByIdUseCase);
 
     return getCarByIdController;
+};
+
+export const makeGetCarsByUserIdController = () => {
+    const postgresGetCarsByUserIdRepository =
+        new PostgresGetCarsByUserIdRepository();
+    const postgresGetUserByIdRepository = new PostgresGetUserById();
+
+    const getCarsByUserIdUseCase = new GetCarsByUserIdUseCase(
+        postgresGetUserByIdRepository,
+        postgresGetCarsByUserIdRepository
+    );
+
+    const getCarsByUserIdController = new GetCarsByUserIdController(
+        getCarsByUserIdUseCase
+    );
+
+    return getCarsByUserIdController;
 };
