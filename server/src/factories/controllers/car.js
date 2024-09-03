6ae4f5c1e756d09d127a797d39323463ad4c76e0
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-03 16:30:59 -0300

    feat: creates function to make update car controller

--- server/src/factories/controllers/car.js
+++ server/src/factories/controllers/car.js
@@ -1,13 +1,16 @@
 import { CreateCarController } from '../../controllers/car/create-car.js';
 import { GetCarByIdController } from '../../controllers/car/get-car-by-id.js';
 import { GetCarsByUserIdController } from '../../controllers/car/get-cars-by-user-id.js';
+import { UpdateCarController } from '../../controllers/car/update-car.js';
 import { PostgresCreateCarRepository } from '../../respositories/postgres/car/create-car.js';
 import { PostgresGetCarByIdRepository } from '../../respositories/postgres/car/get-car-by-id.js';
 import { PostgresGetCarsByUserIdRepository } from '../../respositories/postgres/car/get-cars-by-user-id.js';
+import { PostgresUpdateCarRepository } from '../../respositories/postgres/car/update-car.js';
 import { PostgresGetUserById } from '../../respositories/postgres/user/get-user-by-id.js';
 import { CreateCarUseCase } from '../../use-cases/car/create-car.js';
 import { GetCarByIdUseCase } from '../../use-cases/car/get-car-by-id.js';
 import { GetCarsByUserIdUseCase } from '../../use-cases/car/get-cars-by-user-id.js';
+import { UpdateCarUseCase } from '../../use-cases/car/update-car.js';
 
 export const makeCreateCarController = () => {
     const postgresGetUserByIdRepository = new PostgresGetUserById();
@@ -48,4 +51,18 @@ export const makeGetCarsByUserIdController = () => {
     );
 
     return getCarsByUserIdController;
+};
+
+export const makeUpdateCarController = () => {
+    const postgresGetCarByIdRepository = new PostgresGetCarByIdRepository();
+    const postgresUpdateCarRepository = new PostgresUpdateCarRepository();
+
+    const updateCarUseCase = new UpdateCarUseCase(
+        postgresGetCarByIdRepository,
+        postgresUpdateCarRepository
+    );
+
+    const updateCarController = new UpdateCarController(updateCarUseCase);
+
+    return updateCarController;
 };
