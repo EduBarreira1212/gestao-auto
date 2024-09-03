52f54fb0afcad6c1a7633d6e7044ae3bb470562d
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-03 18:15:11 -0300

    feat: creates function to make delete car controller

--- server/src/factories/controllers/car.js
+++ server/src/factories/controllers/car.js
@@ -1,13 +1,16 @@
 import { CreateCarController } from '../../controllers/car/create-car.js';
+import { DeleteCarController } from '../../controllers/car/delete-car.js';
 import { GetCarByIdController } from '../../controllers/car/get-car-by-id.js';
 import { GetCarsByUserIdController } from '../../controllers/car/get-cars-by-user-id.js';
 import { UpdateCarController } from '../../controllers/car/update-car.js';
 import { PostgresCreateCarRepository } from '../../respositories/postgres/car/create-car.js';
+import { PostgresDeleteCarRepository } from '../../respositories/postgres/car/delete-car.js';
 import { PostgresGetCarByIdRepository } from '../../respositories/postgres/car/get-car-by-id.js';
 import { PostgresGetCarsByUserIdRepository } from '../../respositories/postgres/car/get-cars-by-user-id.js';
 import { PostgresUpdateCarRepository } from '../../respositories/postgres/car/update-car.js';
 import { PostgresGetUserById } from '../../respositories/postgres/user/get-user-by-id.js';
 import { CreateCarUseCase } from '../../use-cases/car/create-car.js';
+import { DeleteCarUseCase } from '../../use-cases/car/delete-car.js';
 import { GetCarByIdUseCase } from '../../use-cases/car/get-car-by-id.js';
 import { GetCarsByUserIdUseCase } from '../../use-cases/car/get-cars-by-user-id.js';
 import { UpdateCarUseCase } from '../../use-cases/car/update-car.js';
@@ -65,4 +68,18 @@ export const makeUpdateCarController = () => {
     const updateCarController = new UpdateCarController(updateCarUseCase);
 
     return updateCarController;
+};
+
+export const makeDeleteCarController = () => {
+    const postgresGetCarByIdRepository = new PostgresGetCarByIdRepository();
+    const postgresDeleteCarRepository = new PostgresDeleteCarRepository();
+
+    const deleteCarUseCase = new DeleteCarUseCase(
+        postgresGetCarByIdRepository,
+        postgresDeleteCarRepository
+    );
+
+    const deleteCarController = new DeleteCarController(deleteCarUseCase);
+
+    return deleteCarController;
 };
