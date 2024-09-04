ff7bae1fdebe60bd3b5fd2c8fe04067e27f179e0
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-04 11:48:13 -0300

    feat: creates create and update car zod's schema

---
+++ server/src/schemas/car.js
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+export const createCarSchema = z.object({
+    user_id: z
+        .string({ required_error: 'User ID is required.' })
+        .uuid({ message: 'User ID must be a valid UUID.' }),
+    brand: z
+        .string({ required_error: 'Car brand is required.' })
+        .trim()
+        .min(1, { message: 'Car brand cannot be empty or just spaces.' }),
+    name: z
+        .string({ required_error: 'Car name is required.' })
+        .trim()
+        .min(1, { message: 'Car name cannot be empty or just spaces.' }),
+    year: z
+        .number({ required_error: 'Car manufacturing year is required.' })
+        .positive({ message: 'Car year must be a positive number.' }),
+    plate: z
+        .string({ required_error: 'License plate is required.' })
+        .trim()
+        .min(7, { message: 'License plate must be exactly 7 characters.' })
+        .max(7, { message: 'License plate must be exactly 7 characters.' }),
+    entry_price: z
+        .number({ required_error: 'Entry price is required.' })
+        .positive({ message: 'Entry price must be a positive number.' }),
+    expenses: z
+        .number()
+        .positive({ message: 'Entry price must be a positive number.' })
+        .optional(),
+});
+
+export const updateCarSchema = createCarSchema.partial().strict({
+    message: 'Some provided field is not allowed',
+});
