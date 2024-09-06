b558acd63fc8e5b0c8d911d91e61d71b359b68d2
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-06 14:43:02 -0300

    feat: creates a function to make GetSellsByUserIdController

--- server/src/factories/controllers/sell.js
+++ server/src/factories/controllers/sell.js
@@ -1,11 +1,14 @@
 import { CreateSellController } from '../../controllers/sell/create-sell.js';
 import { GetSellByIdController } from '../../controllers/sell/get-sell-by-id.js';
+import { GetSellsByUserIdController } from '../../controllers/sell/get-sells-by-user-id.js';
 import { PostgresGetCarByIdRepository } from '../../respositories/postgres/car/get-car-by-id.js';
 import { PostgresCreateSellRepository } from '../../respositories/postgres/sell/create-sell.js';
 import { PostgresGetSellByIdRepository } from '../../respositories/postgres/sell/get-sell-by-id.js';
+import { PostgresGetSellsByUserIddRepository } from '../../respositories/postgres/sell/get-sells-by-user-id.js';
 import { PostgresGetUserById } from '../../respositories/postgres/user/get-user-by-id.js';
 import { CreateSellUseCase } from '../../use-cases/sell/create-sell.js';
 import { GetSellByIdUseCase } from '../../use-cases/sell/get-sell-by-id.js';
+import { GetSellsByUserIdUseCase } from '../../use-cases/sell/get-sells-by-user-id.js';
 
 export const makeCreateSellController = () => {
     const postgresGetUserByIdRepository = new PostgresGetUserById();
@@ -31,4 +34,21 @@ export const makeGetSellByIdController = () => {
     const getSellByIdController = new GetSellByIdController(getSellByIdUseCase);
 
     return getSellByIdController;
+};
+
+export const makeGetSellsByUserIdController = () => {
+    const postgresGetUserByIdRepository = new PostgresGetUserById();
+    const postgresGetSellsByUserIdRepository =
+        new PostgresGetSellsByUserIddRepository();
+
+    const getSellsByUserIdUseCase = new GetSellsByUserIdUseCase(
+        postgresGetUserByIdRepository,
+        postgresGetSellsByUserIdRepository
+    );
+
+    const getSellsByUserIdController = new GetSellsByUserIdController(
+        getSellsByUserIdUseCase
+    );
+
+    return getSellsByUserIdController;
 };
