b4b7bfd44cb69300d69720b9a7ab48d5d4c38d90
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-06 16:40:41 -0300

    feat: creates a function to make deleteSellController

--- server/src/factories/controllers/sell.js
+++ server/src/factories/controllers/sell.js
@@ -1,14 +1,17 @@
 import { CreateSellController } from '../../controllers/sell/create-sell.js';
+import { DeleteSellController } from '../../controllers/sell/delete-sell.js';
 import { GetSellByIdController } from '../../controllers/sell/get-sell-by-id.js';
 import { GetSellsByUserIdController } from '../../controllers/sell/get-sells-by-user-id.js';
 import { UpdateSellController } from '../../controllers/sell/update-sell.js';
 import { PostgresGetCarByIdRepository } from '../../respositories/postgres/car/get-car-by-id.js';
 import { PostgresCreateSellRepository } from '../../respositories/postgres/sell/create-sell.js';
+import { PostgresDeleteSellRepository } from '../../respositories/postgres/sell/delete-sell.js';
 import { PostgresGetSellByIdRepository } from '../../respositories/postgres/sell/get-sell-by-id.js';
 import { PostgresGetSellsByUserIddRepository } from '../../respositories/postgres/sell/get-sells-by-user-id.js';
 import { PostgresUpdateSellRepository } from '../../respositories/postgres/sell/update-sell.js';
 import { PostgresGetUserById } from '../../respositories/postgres/user/get-user-by-id.js';
 import { CreateSellUseCase } from '../../use-cases/sell/create-sell.js';
+import { DeleteSellUseCase } from '../../use-cases/sell/delete-sell.js';
 import { GetSellByIdUseCase } from '../../use-cases/sell/get-sell-by-id.js';
 import { GetSellsByUserIdUseCase } from '../../use-cases/sell/get-sells-by-user-id.js';
 import { UpdateSellUseCase } from '../../use-cases/sell/update-sell.js';
@@ -68,4 +71,18 @@ export const makeUpdateSellController = () => {
     const updateSellController = new UpdateSellController(updateSellUseCase);
 
     return updateSellController;
+};
+
+export const makeDeleteSellController = () => {
+    const postgresGetSellByIdRepository = new PostgresGetSellByIdRepository();
+    const postgresDeleteSellRepository = new PostgresDeleteSellRepository();
+
+    const deleteSellUseCase = new DeleteSellUseCase(
+        postgresGetSellByIdRepository,
+        postgresDeleteSellRepository
+    );
+
+    const deleteSellController = new DeleteSellController(deleteSellUseCase);
+
+    return deleteSellController;
 };
