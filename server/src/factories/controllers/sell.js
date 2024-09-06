ef84c73070db247bdc9f7b993c26cc3e19e9bcd2
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-06 16:11:01 -0300

    feat: creates a function to make updateSellController

--- server/src/factories/controllers/sell.js
+++ server/src/factories/controllers/sell.js
@@ -1,14 +1,17 @@
 import { CreateSellController } from '../../controllers/sell/create-sell.js';
 import { GetSellByIdController } from '../../controllers/sell/get-sell-by-id.js';
 import { GetSellsByUserIdController } from '../../controllers/sell/get-sells-by-user-id.js';
+import { UpdateSellController } from '../../controllers/sell/update-sell.js';
 import { PostgresGetCarByIdRepository } from '../../respositories/postgres/car/get-car-by-id.js';
 import { PostgresCreateSellRepository } from '../../respositories/postgres/sell/create-sell.js';
 import { PostgresGetSellByIdRepository } from '../../respositories/postgres/sell/get-sell-by-id.js';
 import { PostgresGetSellsByUserIddRepository } from '../../respositories/postgres/sell/get-sells-by-user-id.js';
+import { PostgresUpdateSellRepository } from '../../respositories/postgres/sell/update-sell.js';
 import { PostgresGetUserById } from '../../respositories/postgres/user/get-user-by-id.js';
 import { CreateSellUseCase } from '../../use-cases/sell/create-sell.js';
 import { GetSellByIdUseCase } from '../../use-cases/sell/get-sell-by-id.js';
 import { GetSellsByUserIdUseCase } from '../../use-cases/sell/get-sells-by-user-id.js';
+import { UpdateSellUseCase } from '../../use-cases/sell/update-sell.js';
 
 export const makeCreateSellController = () => {
     const postgresGetUserByIdRepository = new PostgresGetUserById();
@@ -51,4 +54,18 @@ export const makeGetSellsByUserIdController = () => {
     );
 
     return getSellsByUserIdController;
+};
+
+export const makeUpdateSellController = () => {
+    const postgresGetSellByIdRepository = new PostgresGetSellByIdRepository();
+    const postgresUpdateSellRepository = new PostgresUpdateSellRepository();
+
+    const updateSellUseCase = new UpdateSellUseCase(
+        postgresGetSellByIdRepository,
+        postgresUpdateSellRepository
+    );
+
+    const updateSellController = new UpdateSellController(updateSellUseCase);
+
+    return updateSellController;
 };
