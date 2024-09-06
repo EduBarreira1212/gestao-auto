057e43a0461d30c84090c7cf9f970e62d742819e
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-05 22:27:58 -0300

    feat: add create sell's schema

---
+++ server/src/schemas/sell.js
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const createSellSchema = z.object({
+    user_id: z
+        .string({ required_error: 'User ID is required.' })
+        .uuid({ message: 'User ID must be a valid UUID.' }),
+    car_id: z
+        .string({ required_error: 'Car ID is required.' })
+        .uuid({ message: 'Car ID must be a valid UUID.' }),
+    amount: z
+        .number({ required_error: 'Sell amount is required.' })
+        .positive({ message: 'Amount must be a positive number.' }),
+    profit: z.number({ required_error: 'Profit is required.' }),
+});
