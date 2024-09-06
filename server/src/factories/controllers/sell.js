dbbd9f225b428c351a7e7ba0d6a0596614514038
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-06 13:22:15 -0300

    feat: create function to make getSellByIdController

--- server/src/factories/controllers/sell.js
+++ server/src/factories/controllers/sell.js
@@ -1,8 +1,11 @@
 import { CreateSellController } from '../../controllers/sell/create-sell.js';
+import { GetSellByIdController } from '../../controllers/sell/get-sell-by-id.js';
 import { PostgresGetCarByIdRepository } from '../../respositories/postgres/car/get-car-by-id.js';
 import { PostgresCreateSellRepository } from '../../respositories/postgres/sell/create-sell.js';
+import { PostgresGetSellByIdRepository } from '../../respositories/postgres/sell/get-sell-by-id.js';
 import { PostgresGetUserById } from '../../respositories/postgres/user/get-user-by-id.js';
 import { CreateSellUseCase } from '../../use-cases/sell/create-sell.js';
+import { GetSellByIdUseCase } from '../../use-cases/sell/get-sell-by-id.js';
 
 export const makeCreateSellController = () => {
     const postgresGetUserByIdRepository = new PostgresGetUserById();
@@ -18,4 +21,14 @@ export const makeCreateSellController = () => {
     const createSellController = new CreateSellController(createSellUseCase);
 
     return createSellController;
+};
+
+export const makeGetSellByIdController = () => {
+    const postgresGetSellByIdRepository = new PostgresGetSellByIdRepository();
+
+    const getSellByIdUseCase = new GetSellByIdUseCase(postgresGetSellByIdRepository);
+
+    const getSellByIdController = new GetSellByIdController(getSellByIdUseCase);
+
+    return getSellByIdController;
 };
