5978c7f1d80ed330a6b399f76f50bd0ccede31a4
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-07 20:55:50 -0300

    test: create user creating test

---
+++ server/src/controllers/user/create-user.test.js
@@ -0,0 +1,27 @@
+import { CreateUserController } from './create-user.js';
+
+describe('CreateUserController', () => {
+    class CreateUserUseCaseStub {
+        execute(user) {
+            return user;
+        }
+    }
+
+    const createUserUseCaseStub = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCaseStub);
+
+    test('should return 201 and body when user is created sucessfully', async () => {
+        const httpRequest = {
+            body: {
+                name: 'Eduardo',
+                email: 'edu@gmail.com',
+                password: '12345',
+            },
+        };
+
+        const result = await createUserController.execute(httpRequest);
+
+        expect(result.statusCode).toBe(201);
+        expect(result.body).toBe(httpRequest.body);
+    });
+});
