8f3868f0b29b4f29e6165435692d6dbc7acd013d
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-08 19:52:55 -0300

    test: changing test name

--- server/src/controllers/user/create-user.test.js
+++ server/src/controllers/user/create-user.test.js
@@ -14,7 +14,7 @@ describe('CreateUserController', () => {
         return sut;
     };
 
-    test('should return 201 and body when user is created sucessfully', async () => {
+    test('should return 201 and body when user is created with sucess', async () => {
         const sut = makeSut();
 
         const httpRequest = {
