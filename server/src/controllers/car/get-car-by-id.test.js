2a03c6a2efd9ef5c01f385a1b14aca4815152bb0
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-09 14:37:08 -0300

    test: add test to GetCarByIdController

---
+++ server/src/controllers/car/get-car-by-id.test.js
@@ -0,0 +1,90 @@
+import { GetCarByIdController } from './get-car-by-id.js';
+
+describe('GetUserByIdController', () => {
+    class GetCarByIdUseCaseStub {
+        async execute(carId) {
+            const car = {
+                user_id: carId,
+                brand: 'Ferrari',
+                name: '488',
+                year: 2018,
+                plate: 'FFF0F00',
+                entry_price: 2500000,
+            };
+            return car;
+        }
+    }
+
+    const makeSut = () => {
+        const getCarByIdUseCase = new GetCarByIdUseCaseStub();
+        const sut = new GetCarByIdController(getCarByIdUseCase);
+
+        return sut;
+    };
+
+    test('should return 200 when car is found', async () => {
+        const sut = makeSut();
+
+        const httpRequest = {
+            params: {
+                carId: '6625edd1-2b56-42f9-84b4-2f86ba234c41',
+            },
+        };
+
+        const result = await sut.execute(httpRequest);
+
+        expect(result.statusCode).toBe(200);
+        expect(result.body.id).toBe(httpRequest.params.userId);
+    });
+
+    test('should return 400 when id is not valid', async () => {
+        const sut = makeSut();
+
+        const httpRequest = {
+            params: {
+                carId: 'invalid_id',
+            },
+        };
+
+        const result = await sut.execute(httpRequest);
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    test('should return 404 when car is not found', async () => {
+        const sut = makeSut();
+
+        const httpRequest = {
+            params: {
+                carId: '6625edd1-2b56-42f9-84b4-2f86ba234c41',
+            },
+        };
+
+        jest.spyOn(sut.getCarByIdUseCase, 'execute').mockImplementationOnce(() => {
+            return null;
+        });
+
+        const result = await sut.execute(httpRequest);
+
+        expect(result.statusCode).toBe(404);
+        expect(result.body.message).toBe('Car not found');
+    });
+
+    test('should return 500 when execute throws an error', async () => {
+        const sut = makeSut();
+
+        const httpRequest = {
+            params: {
+                carId: '6625edd1-2b56-42f9-84b4-2f86ba234c41',
+            },
+        };
+
+        jest.spyOn(sut.getCarByIdUseCase, 'execute').mockImplementationOnce(() => {
+            throw new Error();
+        });
+
+        const result = await sut.execute(httpRequest);
+
+        expect(result.statusCode).toBe(500);
+    });
+});
