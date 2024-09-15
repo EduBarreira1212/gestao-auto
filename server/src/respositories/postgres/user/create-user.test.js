0f05a2938cfdf03ebe409b2dd8d3b178610d2705
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-15 16:51:57 -0300

    test: add test to PostgresCreateUserRepository

---
+++ server/src/respositories/postgres/user/create-user.test.js
@@ -0,0 +1,23 @@
+import { PostgresCreateUserRepositorie } from './create-user.js';
+import { userFixture as user } from '../../../tests/fixtures/user.js';
+
+describe('PostgresCreateUserRepository', () => {
+    const sut = new PostgresCreateUserRepositorie();
+
+    test('should create a user sucessfully', async () => {
+        const result = await sut.execute(user);
+
+        expect(result).not.toBeFalsy();
+    });
+
+    test('should return a user with correct properties', async () => {
+        const result = await sut.execute(user);
+
+        expect(result).toStrictEqual({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+        });
+    });
+});
