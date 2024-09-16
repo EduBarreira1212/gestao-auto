ceeaa049b6110757ae2a256f6c9d072344db07d8
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-16 10:25:09 -0300

    test: add test to PostgresGetCarByIdRepository

---
+++ server/src/respositories/postgres/car/get-car-by-id.test.js
@@ -0,0 +1,35 @@
+import { PostgresGetCarByIdRepository } from './get-car-by-id.js';
+import { userFixture as user } from '../../../tests/fixtures/user.js';
+import { carFixture as car } from '../../../tests/fixtures/car.js';
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+describe('PostgresGetCarByIdRepository', () => {
+    const sut = new PostgresGetCarByIdRepository();
+
+    test('should get a car sucessfully', async () => {
+        await prisma.user.create({ data: user });
+        await prisma.car.create({ data: { ...car, user_id: user.id } });
+        const result = await sut.execute(car.id);
+
+        expect(result).not.toBeFalsy();
+    });
+
+    test('should return a car with correct properties', async () => {
+        await prisma.user.create({ data: user });
+        await prisma.car.create({ data: { ...car, user_id: user.id } });
+        const result = await sut.execute(car.id);
+
+        expect(result).toStrictEqual({
+            id: car.id,
+            user_id: user.id,
+            brand: car.brand,
+            name: car.name,
+            year: car.year,
+            plate: car.plate,
+            expenses: 0,
+            entry_price: car.entry_price,
+        });
+    });
+});
