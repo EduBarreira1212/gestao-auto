48797dde53d6d3202decc95b335a3f8e67e31c85
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-16 11:47:27 -0300

    test: add test to PostgresGetSellsByUserIdRepository

---
+++ server/src/respositories/postgres/sell/get-sells-by-user-id.test.js
@@ -0,0 +1,43 @@
+import { PostgresGetSellsByUserIddRepository } from './get-sells-by-user-id.js';
+import { userFixture as user } from '../../../tests/fixtures/user.js';
+import { carFixture as car } from '../../../tests/fixtures/car.js';
+import { sellFixture as sell } from '../../../tests/fixtures/sell.js';
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+describe('PostgresGetSellsByUserIdRepository', () => {
+    const sut = new PostgresGetSellsByUserIddRepository();
+
+    test('should get sells sucessfully', async () => {
+        await prisma.user.create({ data: user });
+        await prisma.car.create({ data: { ...car, user_id: user.id } });
+        await prisma.sell.create({
+            data: { ...sell, user_id: user.id, car_id: car.id },
+        });
+
+        const result = await sut.execute(user.id);
+
+        expect(result).not.toBeFalsy();
+    });
+
+    test('should return a array of sells with correct properties', async () => {
+        await prisma.user.create({ data: user });
+        await prisma.car.create({ data: { ...car, user_id: user.id } });
+        await prisma.sell.create({
+            data: { ...sell, user_id: user.id, car_id: car.id },
+        });
+
+        const result = await sut.execute(user.id);
+
+        expect(result).toStrictEqual([
+            {
+                id: sell.id,
+                user_id: user.id,
+                car_id: car.id,
+                amount: sell.amount,
+                profit: sell.profit,
+            },
+        ]);
+    });
+});
