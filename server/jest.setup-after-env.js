06a874432f0bdddbfe8b548587ca72221f46949d
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-15 23:35:29 -0300

    fix: changes delete order

--- server/jest.setup-after-env.js
+++ server/jest.setup-after-env.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 beforeEach(async () => {
-    await prisma.user.deleteMany({});
-    await prisma.car.deleteMany({});
     await prisma.sell.deleteMany({});
+    await prisma.car.deleteMany({});
+    await prisma.user.deleteMany({});
 });
