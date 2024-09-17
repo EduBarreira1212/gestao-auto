6ef19d1208a5fc12faf6bdbc2dc2a84de842e43f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-17 12:19:28 -0300

    refactor: uses only the router  in the index

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import {
-    makeCreateUserController,
-    makeDeleteUserController,
-    makeGetUserByIdController,
-    makeUpdateUserController,
-} from './factories/controllers/user.js';
-
-import {
-    makeCreateCarController,
-    makeDeleteCarController,
-    makeGetCarByIdController,
-    makeGetCarsByUserIdController,
-    makeUpdateCarController,
-} from './factories/controllers/car.js';
-import {
-    makeCreateSellController,
-    makeDeleteSellController,
-    makeGetSellByIdController,
-    makeGetSellsByUserIdController,
-    makeUpdateSellController,
-} from './factories/controllers/sell.js';
+import { usersRouter } from './routes/user.js';
+import { carsRouter } from './routes/car.js';
+import { sellsRouter } from './routes/sell.js';
 
 dotenv.config({ path: '.env' });
 
@@ -29,117 +11,9 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/api/get-user/:userId', async (req, res) => {
-    const getUserById = makeGetUserByIdController();
-
-    const { statusCode, body } = await getUserById.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.post('/api/create-user', async (req, res) => {
-    const createUserController = makeCreateUserController();
-
-    const { statusCode, body } = await createUserController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.patch('/api/update-user/:userId', async (req, res) => {
-    const updateUserController = makeUpdateUserController();
-
-    const { statusCode, body } = await updateUserController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.delete('/api/delete-user/:userId', async (req, res) => {
-    const deleteUserController = makeDeleteUserController();
-
-    const { statusCode, body } = await deleteUserController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.get('/api/get-car/:carId', async (req, res) => {
-    const getCarByIdController = makeGetCarByIdController();
-
-    const { statusCode, body } = await getCarByIdController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.get('/api/get-cars', async (req, res) => {
-    const getCarsByUserIdController = makeGetCarsByUserIdController();
-
-    const { statusCode, body } = await getCarsByUserIdController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.post('/api/create-car', async (req, res) => {
-    const createCarController = makeCreateCarController();
-
-    const { statusCode, body } = await createCarController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.patch('/api/update-car/:carId', async (req, res) => {
-    const updateCarController = makeUpdateCarController();
-
-    const { statusCode, body } = await updateCarController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.delete('/api/delete-car/:carId', async (req, res) => {
-    const deleteCarController = makeDeleteCarController();
-
-    const { statusCode, body } = await deleteCarController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.get('/api/get-sell/:sellId', async (req, res) => {
-    const getSellByIdController = makeGetSellByIdController();
-
-    const { statusCode, body } = await getSellByIdController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.get('/api/get-sells', async (req, res) => {
-    const getSellsByUserIdController = makeGetSellsByUserIdController();
-
-    const { statusCode, body } = await getSellsByUserIdController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.post('/api/create-sell', async (req, res) => {
-    const createSellController = makeCreateSellController();
-
-    const { statusCode, body } = await createSellController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.patch('/api/update-sell/:sellId', async (req, res) => {
-    const updateSellController = makeUpdateSellController();
-
-    const { statusCode, body } = await updateSellController.execute(req);
-
-    res.status(statusCode).send(body);
-});
-
-app.delete('/api/delete-sell/:sellId', async (req, res) => {
-    const deleteSellController = makeDeleteSellController();
-
-    const { statusCode, body } = await deleteSellController.execute(req);
-
-    res.status(statusCode).send(body);
-});
+app.use('/api/users', usersRouter);
+app.use('/api/cars', carsRouter);
+app.use('/api/sells', sellsRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
