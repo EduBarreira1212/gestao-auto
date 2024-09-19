80fffb60684831a519a0eaa069ba37aefc03ddc1
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-19 11:46:13 -0300

    feat: create a route to acess doc

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
 
 import { usersRouter } from './routes/user.js';
 import { carsRouter } from './routes/car.js';
@@ -11,3 +14,9 @@ app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/cars', carsRouter);
 app.use('/api/sells', sellsRouter);
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.join(import.meta.dirname, '../docs/swagger.json'), 'utf8')
+);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
