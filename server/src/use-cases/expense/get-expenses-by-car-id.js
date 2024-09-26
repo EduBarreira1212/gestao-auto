5f23f0b74549858b9c18de737d37dc2bf59d251a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-09-26 19:11:15 -0300

    feat: creates PostgresGetExpensesByCarIdUseCase

---
+++ server/src/use-cases/expense/get-expenses-by-car-id.js
@@ -0,0 +1,18 @@
+export class GetExpensesByCarIdRepository {
+    constructor(getCarByIdRepository, getExpensesByCarIdRepository) {
+        this.getCarByIdRepository = getCarByIdRepository;
+        this.getExpensesByCarIdRepository = getExpensesByCarIdRepository;
+    }
+    async execute(carId) {
+        const carWithProvidedIdExists =
+            await this.getCarByIdRepository.execute(carId);
+
+        if (!carWithProvidedIdExists) {
+            throw new Error('Car with provided ID do not exists');
+        }
+
+        const expenses = await this.getExpensesByCarIdRepository.execute(carId);
+
+        return expenses;
+    }
+}
