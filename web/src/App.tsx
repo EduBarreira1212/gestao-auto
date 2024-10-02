a15b81309fdc4eb71dd851823f7b988927bba77c
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-02 18:56:25 -0300

    style: apply prettier format

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,9 @@
-
 const App = () => {
-  return (
-    <>
-      <h1 className="text-5xl text-center">CARS-CRM</h1>
-    </>
-  )
-}
+    return (
+        <>
+            <h1 className="text-5xl text-center">CARS-CRM</h1>
+        </>
+    );
+};
 
 export default App;
