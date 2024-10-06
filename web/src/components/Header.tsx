cca26c1f983af49b58288d9e7f72ea33d364e8da
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-06 19:52:06 -0300

    feat: add pages names and user arrow icon

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,14 +1,21 @@
+import arrowIcon from '../assets/setaBaixo.png';
+
 const Header = () => {
     return (
         <header className="flex flex-row justify-between bg-gray-200 px-8 py-3">
             <h2>Page name</h2>
             <div className="flex gap-4">
-                <a href="#">Page</a>
-                <a href="#">Page</a>
-                <a href="#">Page</a>
-                <a href="#">Page</a>
+                <a href="#">Vendas</a>
+                <a href="#">Veículos</a>
+                <a href="#">Despesas</a>
+                <a href="#">Leads</a>
             </div>
-            <div>username</div>
+            <div className="flex items-center gap-2">
+                username
+                <button>
+                    <img src={arrowIcon} className="h-4" />
+                </button>
+            </div>
         </header>
     );
 };
