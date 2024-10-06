4494a58ed674316b7d14c689e5709ca7f4332676
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-06 19:51:26 -0300

    fix: define logo size and take out unnecessary anchor tags

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,27 +1,31 @@
+import logo from '../assets/logo.png';
 import moneyIcon from '../assets/saco-de-dolar.png';
 import carIcon from '../assets/carro-alt.png';
+import graphicIcon from '../assets/graphicicon.png';
+import leadsIcon from '../assets/leads.png';
 import ANavbar from './ANavbar';
 
 const Navbar = () => {
     return (
         <nav className="flex h-screen flex-col items-center gap-8 bg-green-400 p-5">
             <div>
-                <a href="#">Logo</a>
+                <a href="#">
+                    <img src={logo} alt="logo" className="h-16" />
+                </a>
             </div>
             <div className="flex flex-col items-center gap-5 border-y-2 p-5">
-                <ANavbar href="#" iconURL={moneyIcon}>
+                <ANavbar href="#" iconURL={graphicIcon}>
                     Vendas
                 </ANavbar>
                 <ANavbar href="#" iconURL={carIcon}>
                     Veículos
                 </ANavbar>
-                <a href="#">Anúncios</a>
-                <a href="#">Leads</a>
-            </div>
-            <div className="flex flex-col items-center gap-5 py-5">
-                <a href="#">None</a>
-                <a href="#">None</a>
-                <a href="#">None</a>
+                <ANavbar href="#" iconURL={moneyIcon}>
+                    Despesas
+                </ANavbar>
+                <ANavbar href="#" iconURL={leadsIcon}>
+                    Leads
+                </ANavbar>
             </div>
         </nav>
     );
