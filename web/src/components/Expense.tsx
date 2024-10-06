2c712280a37f86ba30ea21b31e329c35b1cfcde7
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-06 19:52:50 -0300

    feat: create first version of expense component

---
+++ web/src/components/Expense.tsx
@@ -0,0 +1,12 @@
+const Expense = () => {
+    return (
+        <div className="flex w-fit flex-col gap-3 rounded-md border-solid bg-slate-100 p-5 text-center shadow-md">
+            <span>Car</span>
+            <span>Amount</span>
+            <span>description</span>
+            <span>Data da despesa: 01/01/2000</span>
+        </div>
+    );
+};
+
+export default Expense;
