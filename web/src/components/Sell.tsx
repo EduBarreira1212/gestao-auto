ec89c3403aa693b28ab58f9283304cead97fc19d
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-06 19:53:03 -0300

    feat: create first version of sell component

---
+++ web/src/components/Sell.tsx
@@ -0,0 +1,13 @@
+const Sell = () => {
+    return (
+        <div className="flex w-fit flex-col gap-3 rounded-md border-solid bg-slate-100 p-5 text-center shadow-md">
+            <span>Car</span>
+            <span>Lead</span>
+            <span>Total</span>
+            <span>Profit</span>
+            <span>Data da venda: 01/01/2000</span>
+        </div>
+    );
+};
+
+export default Sell;
