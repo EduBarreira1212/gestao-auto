4e598bfd34b69e6b02c3aaa3073c6a9d18857623
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-07 18:29:04 -0300

    feat: render components in app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,20 @@
+import Expense from './components/Expense';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Sell from './components/Sell';
+import Vehicle from './components/Vehicle';
 
 const App = () => {
     return (
         <div className="flex flex-row">
             <Navbar />
-            <div className="w-full">
+            <div className="bg-brand-neutral w-full">
                 <Header />
+                <div className="p-3">
+                    <Vehicle />
+                    <Sell />
+                    <Expense />
+                </div>
             </div>
         </div>
     );
