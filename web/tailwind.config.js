265912aefd06fadbddb5fc8a49377d8a89d1ac81
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-07 18:27:07 -0300

    feat: create desing system using tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./index.html', './src/**/*.{ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    primary: '#00ADB5',
+                    secondary: '#0A0F29',
+                    accent: '#FF5722',
+                    neutral: '#EEEEEE',
+                },
+            },
+        },
+    },
+    plugins: [],
+};
