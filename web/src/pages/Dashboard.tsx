b4733ce9005db7d17e2ed73f8a3206becc4da328
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-08 19:19:26 -0300

    feat: create initial version of dashboard

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -3,11 +3,19 @@ import Navbar from '../components/Navbar';
 
 const Dashboard = () => {
     return (
-        <div className="flex flex-row">
+        <div className="flex h-screen w-full flex-row">
             <Navbar />
-            <div className="w-full bg-brand-neutral">
+            <div className="flex w-full flex-col bg-brand-neutral">
                 <Header />
-                <div className="p-3"></div>
+                <div className="flex h-full flex-row items-center justify-center p-3">
+                    <div className="flex flex-col items-center gap-3 rounded-md border-2 border-solid bg-slate-50 p-8 text-brand-secondary shadow-md shadow-brand-primary">
+                        <span>Carros em estoque: 0</span>
+                        <span>Vendas esse mês: 0</span>
+                        <span>Vendas totais: 0</span>
+                        <span>Faturamento: 0</span>
+                        <span>Despesas esse mês: 0</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
