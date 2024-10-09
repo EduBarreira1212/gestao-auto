08a8d07f5892964a1c32ab49bea199606d0aa5e9
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-09 12:17:15 -0300

    feat: use overview component

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import Overview from '../components/Overview';
 
 const Dashboard = () => {
     return (
@@ -8,13 +9,7 @@ const Dashboard = () => {
             <div className="flex w-full flex-col bg-brand-neutral">
                 <Header />
                 <div className="flex h-full flex-row items-center justify-center p-3">
-                    <div className="flex flex-col items-center gap-3 rounded-md border-2 border-solid bg-slate-50 p-8 text-brand-secondary shadow-md shadow-brand-primary">
-                        <span>Carros em estoque: 0</span>
-                        <span>Vendas esse mês: 0</span>
-                        <span>Vendas totais: 0</span>
-                        <span>Faturamento: 0</span>
-                        <span>Despesas esse mês: 0</span>
-                    </div>
+                    <Overview />
                 </div>
             </div>
         </div>
