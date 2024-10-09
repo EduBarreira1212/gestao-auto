ab08371ae368dcfab94dc270fd05fe61293030f2
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-09 12:16:33 -0300

    feat: use button component with useNavigate

--- web/src/components/Vehicle.tsx
+++ web/src/components/Vehicle.tsx
@@ -1,4 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+import Button from './Button';
+
 const Vehicle = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="flex w-fit flex-col gap-3 rounded-md border-2 border-solid bg-slate-50 p-5 text-center text-brand-secondary shadow-md shadow-brand-primary">
             <span>Model</span>
@@ -8,6 +13,10 @@ const Vehicle = () => {
             <span>EntryPrice</span>
             <span>Despesas totais: 0</span>
             <span>Tempo em estoque: 0 Dias</span>
+            <div className="flex flex-row justify-between">
+                <Button onClick={() => navigate('/')}>Ver detalhes</Button>
+                <Button onClick={() => navigate('/')}>Vender</Button>
+            </div>
         </div>
     );
 };
