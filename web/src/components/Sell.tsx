8a1b678aa9149f1ef7bfccd4482729137b06c10c
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-09 12:16:18 -0300

    feat: use button component with useNavigate

--- web/src/components/Sell.tsx
+++ web/src/components/Sell.tsx
@@ -1,4 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+import Button from './Button';
+
 const Sell = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="flex w-fit flex-col gap-3 rounded-md border-2 border-solid bg-slate-50 p-5 text-center text-brand-secondary shadow-md shadow-brand-primary">
             <span>Car</span>
@@ -6,6 +11,7 @@ const Sell = () => {
             <span>Total</span>
             <span>Profit</span>
             <span>Data da venda: 01/01/2000</span>
+            <Button onClick={() => navigate('/')}>Ver detalhes</Button>
         </div>
     );
 };
