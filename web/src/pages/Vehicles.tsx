39fccce1be1ddd198fca3355afdc4319f5ae2a49
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-10 20:07:49 -0300

    feat: create function to get cars of a user and render vehicles components

--- web/src/pages/Vehicles.tsx
+++ web/src/pages/Vehicles.tsx
@@ -1,23 +1,36 @@
+import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Vehicle from '../components/Vehicle';
+import getCarsByUserId from '../services/getCarsByUserId';
 
 const Vehicles = () => {
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        const getCars = async () => {
+            const carList = await getCarsByUserId(
+                '25ce759d-bf3e-4f25-86ff-814839576bf7'
+            );
+
+            console.log(carList);
+            setCars(carList);
+        };
+
+        getCars();
+    }, []);
+
     return (
         <div className="flex h-screen w-full">
             <Navbar />
             <div className="flex w-full flex-col bg-brand-neutral">
                 <Header />
                 <div className="flex h-full flex-1 flex-row flex-wrap items-center justify-center gap-5 overflow-auto p-3">
-                    <Vehicle />
-                    <Vehicle />
-                    <Vehicle />
-                    <Vehicle />
-                    <Vehicle />
-                    <Vehicle />
-                    <Vehicle />
-                    <Vehicle />
-                    <Vehicle />
+                    {cars.map((car, index) => (
+                        <li key={index}>
+                            <Vehicle car={car} />
+                        </li>
+                    ))}
                 </div>
             </div>
         </div>
