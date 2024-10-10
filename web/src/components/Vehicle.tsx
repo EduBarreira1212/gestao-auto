8b821b185cedecc142d0bf5bba79220d36af1acc
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-10 20:06:39 -0300

    feat: add logic to receive car as props and display properties

--- web/src/components/Vehicle.tsx
+++ web/src/components/Vehicle.tsx
@@ -1,18 +1,32 @@
 import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 
-const Vehicle = () => {
+type ICar = {
+    car: {
+        id: string;
+        user_id: string;
+        name: string;
+        brand: string;
+        year: number;
+        plate: string;
+        entry_price: number;
+        expenses: [];
+        createdAt: string;
+    };
+};
+
+const Vehicle = ({ car }: ICar) => {
     const navigate = useNavigate();
 
     return (
         <div className="flex w-fit flex-col gap-3 rounded-md border-2 border-solid bg-slate-50 p-5 text-center text-brand-secondary shadow-md shadow-brand-primary">
-            <span>Model</span>
-            <span>Brand</span>
-            <span>Year</span>
-            <span>Plate</span>
-            <span>EntryPrice</span>
-            <span>Despesas totais: 0</span>
-            <span>Tempo em estoque: 0 Dias</span>
+            <span>{car.name}</span>
+            <span>{car.brand}</span>
+            <span>{car.year}</span>
+            <span>{car.plate}</span>
+            <span>{car.entry_price}</span>
+            <span>{car.expenses}</span>
+            <span>{car.createdAt}</span>
             <div className="flex flex-row justify-between">
                 <Button onClick={() => navigate('/')}>Ver detalhes</Button>
                 <Button onClick={() => navigate('/')}>Vender</Button>
