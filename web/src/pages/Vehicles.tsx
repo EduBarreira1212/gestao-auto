8314ea8e0722ee0726cce4aee63f4f468ed5993e
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-11 12:46:50 -0300

    feat: creating logic to show a message if array is empty

--- web/src/pages/Vehicles.tsx
+++ web/src/pages/Vehicles.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Vehicle from '../components/Vehicle';
-import getCarsByUserId from '../services/getCarsByUserId';
+import getCarsByUserId from '../services/car/getCarsByUserId';
 
 const Vehicles = () => {
     const [cars, setCars] = useState([]);
@@ -13,7 +13,6 @@ const Vehicles = () => {
                 '25ce759d-bf3e-4f25-86ff-814839576bf7'
             );
 
-            console.log(carList);
             setCars(carList);
         };
 
@@ -26,11 +25,15 @@ const Vehicles = () => {
             <div className="flex w-full flex-col bg-brand-neutral">
                 <Header />
                 <div className="flex h-full flex-1 flex-row flex-wrap items-center justify-center gap-5 overflow-auto p-3">
-                    {cars.map((car, index) => (
-                        <li key={index}>
-                            <Vehicle car={car} />
-                        </li>
-                    ))}
+                    {cars.length > 0 ? (
+                        cars.map((car, index) => (
+                            <li key={index}>
+                                <Vehicle car={car} />
+                            </li>
+                        ))
+                    ) : (
+                        <div>Nenhum veículo encontrado</div>
+                    )}
                 </div>
             </div>
         </div>
