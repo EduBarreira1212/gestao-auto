d2a45e8c4de90ab0bc6a6326cea01d514d9260ae
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-11 19:16:24 -0300

    feat: create new types

---
+++ web/src/types.ts
@@ -0,0 +1,39 @@
+export type CreateCar = {
+    user_id: string;
+    name: string;
+    brand: string;
+    year: number;
+    plate: string;
+    entry_price: number;
+};
+
+export type UpdateCar = {
+    name: string;
+    brand: string;
+    year: number;
+    plate: string;
+    entry_price: number;
+};
+
+export type CreateExpense = {
+    car_id: string;
+    amount: number;
+    description: string;
+};
+
+export type UpdateExpense = {
+    amount: number;
+    description: string;
+};
+
+export type CreateSell = {
+    user_id: string;
+    car_id: string;
+    amount: number;
+    profit: number;
+};
+
+export type UpdateSell = {
+    amount: number;
+    profit: number;
+};
