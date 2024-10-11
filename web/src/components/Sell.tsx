92cb99a0252b8b43cc59ba8a96890270577b8c14
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-11 12:47:13 -0300

    feat: render sell properties

--- web/src/components/Sell.tsx
+++ web/src/components/Sell.tsx
@@ -1,16 +1,26 @@
 import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 
-const Sell = () => {
+type ISell = {
+    sell: {
+        id: string;
+        user_id: string;
+        car_id: string;
+        amount: number;
+        profit: number;
+        createdAt: string;
+    };
+};
+
+const Sell = ({ sell }: ISell) => {
     const navigate = useNavigate();
 
     return (
         <div className="flex w-fit flex-col gap-3 rounded-md border-2 border-solid bg-slate-50 p-5 text-center text-brand-secondary shadow-md shadow-brand-primary">
-            <span>Car</span>
-            <span>Lead</span>
-            <span>Total</span>
-            <span>Profit</span>
-            <span>Data da venda: 01/01/2000</span>
+            <span>Car: {sell.car_id}</span>
+            <span>Total: {sell.amount}</span>
+            <span>Profit: {sell.profit}</span>
+            <span>Data da venda: {sell.createdAt}</span>
             <Button onClick={() => navigate('/')}>Ver detalhes</Button>
         </div>
     );
