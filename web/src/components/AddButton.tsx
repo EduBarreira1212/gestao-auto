eb4985dea2ef0a4a52f582e99313cbd1d9cc3eee
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-12 16:09:08 -0300

    feat: create type for props and receive onClick

--- web/src/components/AddButton.tsx
+++ web/src/components/AddButton.tsx
@@ -1,6 +1,14 @@
-const AddButton = ({ children }: { children: string }) => {
+type AddButtonProps = {
+    children: string;
+    onClick: () => void;
+};
+
+const AddButton = ({ children, onClick }: AddButtonProps) => {
     return (
-        <button className="mt-2 rounded-md border-2 bg-brand-primary p-2 text-brand-neutral hover:text-brand-accent">
+        <button
+            onClick={onClick}
+            className="mt-2 rounded-md border-2 bg-brand-primary p-2 text-brand-neutral hover:text-brand-accent"
+        >
             {children}
         </button>
     );
