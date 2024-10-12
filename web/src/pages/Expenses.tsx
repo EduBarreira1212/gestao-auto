e8288eac58b4c947578bc6b9b5c4c5a115955d0a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-12 16:11:37 -0300

    feat: create logic to render expenses using useState and useEffect

--- web/src/pages/Expenses.tsx
+++ web/src/pages/Expenses.tsx
@@ -1,23 +1,42 @@
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Expense from '../components/Expense';
+import ContentSection from '../components/ContentSection';
+import { useEffect, useState } from 'react';
+import List from '../components/List';
+import getExpensesByCarId from '../services/expense/getExpensesByCarId';
 
 const Expenses = () => {
+    const [expenses, setExpenses] = useState([]);
+
+    useEffect(() => {
+        const getExpenses = async () => {
+            const expenseList = await getExpensesByCarId('');
+
+            setExpenses(expenseList);
+        };
+
+        getExpenses();
+    }, []);
+
     return (
         <div className="flex h-screen w-full flex-row">
             <Navbar />
             <div className="flex w-full flex-col bg-brand-neutral">
                 <Header />
-                <div className="flex h-full flex-1 flex-row flex-wrap items-center justify-center gap-5 overflow-auto p-3">
-                    <Expense />
-                    <Expense />
-                    <Expense />
-                    <Expense />
-                    <Expense />
-                    <Expense />
-                    <Expense />
-                    <Expense />
-                </div>
+                <ContentSection>
+                    {expenses.length > 0 ? (
+                        <List>
+                            {expenses.map((expense, index) => (
+                                <li key={index}>
+                                    <Expense expense={expense} />
+                                </li>
+                            ))}
+                        </List>
+                    ) : (
+                        <div>Nenhuma despesa encontrada</div>
+                    )}
+                </ContentSection>
             </div>
         </div>
     );
