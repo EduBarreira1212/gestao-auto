680193fd4d32c44de7dccea2ddf14b7f774cf865
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-13 22:00:13 -0300

    feat: create dashboard layout

---
+++ web/src/layouts/DashboardLayout.tsx
@@ -0,0 +1,24 @@
+import { useAuth } from '@clerk/clerk-react';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+export default function DashboardLayout() {
+    const { userId, isLoaded } = useAuth();
+    const navigate = useNavigate();
+
+    console.log('test', userId);
+
+    useEffect(() => {
+        if (isLoaded && !userId) {
+            navigate('/sign-in');
+        }
+
+        if (isLoaded && userId) {
+            navigate('/dashboard');
+        }
+    }, [isLoaded]);
+
+    if (!isLoaded) return 'Loading...';
+
+    return <Outlet />;
+}
