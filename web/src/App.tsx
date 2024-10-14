76ad5eb2eb4ff836ce1ec77a734525b24658f57a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-13 22:01:01 -0300

    feat: create correct routes with childrens

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import { SignInButton } from '@clerk/clerk-react';
+
 import Dashboard from './pages/Dashboard';
 import Sells from './pages/Sells';
 import Vehicles from './pages/Vehicles';
 import Expenses from './pages/Expenses';
+import RootLayout from './layouts/RootLayout';
+import DashboardLayout from './layouts/DashboardLayout';
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/vendas" element={<Sells />} />
-            <Route path="/veiculos" element={<Vehicles />} />
-            <Route path="/despesas" element={<Expenses />} />
+            <Route element={<RootLayout />}>
+                <Route path="/" element={<DashboardLayout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/vendas" element={<Sells />} />
+                    <Route path="/veiculos" element={<Vehicles />} />
+                    <Route path="/despesas" element={<Expenses />} />
+                    <Route path="/sign-in" element={<SignInButton />} />
+                </Route>
+            </Route>
         </Routes>
     );
 };
