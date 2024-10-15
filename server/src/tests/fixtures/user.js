a76497612f4f820911665ee01409dd4ea0d79c7b
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-14 23:13:31 -0300

    feat: add external_id to user fixture

--- server/src/tests/fixtures/user.js
+++ server/src/tests/fixtures/user.js
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker';
 
 export const userFixture = {
     id: faker.string.uuid(),
+    external_id: faker.string.alphanumeric({ length: { min: 8, max: 15 } }),
     name: faker.person.firstName(),
     email: faker.internet.email(),
     password: faker.internet.password({ length: 5 }),
