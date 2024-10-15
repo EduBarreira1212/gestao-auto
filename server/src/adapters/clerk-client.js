a7dc1ccc3e447f878fff4ce7c606710179566442
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-14 23:14:41 -0300

    feat: create clerkClient adapter

---
+++ server/src/adapters/clerk-client.js
@@ -0,0 +1,25 @@
+import clerkClient from '../../clerk/clerk.js';
+
+export class ClerkClientAdapter {
+    async createUser(createClerkUserParams) {
+        return await clerkClient.users.createUser(createClerkUserParams);
+    }
+
+    async deleteUser(userId) {
+        return await clerkClient.users.deleteUser(userId);
+    }
+
+    async updateUser(userId, updateUserParams) {
+        return await clerkClient.users.updateUser(userId, updateUserParams);
+    }
+
+    async createEmail(createEmailParams) {
+        return await clerkClient.emailAddresses.createEmailAddress(
+            createEmailParams
+        );
+    }
+
+    async deleteEmail(emailId) {
+        return await clerkClient.emailAddresses.deleteEmailAddress(emailId);
+    }
+}
