c5d3998a3e9c45b7d7745151663359120479792d
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-18 17:56:09 -0300

    refactor: import and use schema from zod schemas file

--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -1,34 +1,11 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
 import { useSignIn } from '@clerk/clerk-react';
 
 import logo from '../assets/logo.png';
 import { CreateUser } from '../types';
 import createUser from '../services/user/createUser';
-
-const schema = z.object({
-    name: z
-        .string()
-        .trim()
-        .min(2, 'O nome deve ter no mínimo 2 caracteres')
-        .max(50, 'O nome não pode exceder 50 caracteres'),
-
-    email: z.string().trim().email('Formato de e-mail inválido'),
-
-    password: z
-        .string()
-        .trim()
-        .min(8, 'A senha deve ter no mínimo 8 caracteres')
-        .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
-        .regex(/[a-z]/, 'A senha deve conter pelo menos uma letra minúscula')
-        .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
-        .regex(
-            /[@$!%*?&]/,
-            'A senha deve conter pelo menos um caractere especial (@, $, !, %, *, ?, &)'
-        )
-        .max(100, 'A senha não pode exceder 100 caracteres'),
-});
+import { signUpSchema } from '../schemas/zodSchemas';
 
 const SignUp = () => {
     const { signIn, setActive } = useSignIn();
@@ -38,7 +15,7 @@ const SignUp = () => {
         handleSubmit,
         formState: { errors },
     } = useForm<CreateUser>({
-        resolver: zodResolver(schema),
+        resolver: zodResolver(signUpSchema),
     });
 
     const onSubmit: SubmitHandler<CreateUser> = async (createUserParams) => {
