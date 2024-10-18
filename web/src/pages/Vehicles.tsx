405350643189693c8485305ab172b75bdf210d32
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-17 22:54:17 -0300

    refactor: use react query to make requests

--- web/src/pages/Vehicles.tsx
+++ web/src/pages/Vehicles.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Vehicle from '../components/Vehicle';
@@ -9,24 +9,37 @@ import List from '../components/List';
 import { createPortal } from 'react-dom';
 import AddVehicleModal from '../components/AddVehicleModal';
 import { useUser } from '@clerk/clerk-react';
+import { useQuery } from '@tanstack/react-query';
+
+export type Vehicle = {
+    id: string;
+    user_id: string;
+    name: string;
+    brand: string;
+    year: number;
+    plate: string;
+    entry_price: number;
+    expenses: [];
+    createdAt: string;
+};
 
 const Vehicles = () => {
-    const [cars, setCars] = useState([]);
     const [showAddVehicleModal, setShowAddVehicleModal] = useState(false);
 
     const { user } = useUser();
 
-    useEffect(() => {
-        if (!user || !user.externalId) return;
+    const { data } = useQuery({
+        queryKey: ['vehicles'],
+        queryFn: async () => {
+            const response = await getCarsByUserId(user?.externalId ?? '');
 
-        const getCars = async () => {
-            const carList = await getCarsByUserId(user.externalId ?? '');
+            if (response?.status !== 200) {
+                throw new Error();
+            }
 
-            setCars(carList);
-        };
-
-        getCars();
-    }, [user]);
+            return response?.data;
+        },
+    });
 
     return (
         <div className="flex h-screen w-full">
@@ -37,11 +50,11 @@ const Vehicles = () => {
                     <AddButton onClick={() => setShowAddVehicleModal(true)}>
                         Adicionar novo veículo
                     </AddButton>
-                    {cars.length > 0 ? (
+                    {data?.length > 0 ? (
                         <List>
-                            {cars.map((car, index) => (
-                                <li key={index}>
-                                    <Vehicle car={car} />
+                            {data.map((vehicle: Vehicle) => (
+                                <li key={vehicle.id}>
+                                    <Vehicle car={vehicle} />
                                 </li>
                             ))}
                         </List>
