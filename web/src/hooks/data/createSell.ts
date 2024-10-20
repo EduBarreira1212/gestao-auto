cedd6997c750b91c8acb5f43ad3479b706b20b0a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-20 19:17:56 -0300

    feat: create hook to create sells

---
+++ web/src/hooks/data/createSell.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { CreateSell, SellType } from '../../types';
+import createSell from '../../services/sell/createSell';
+
+export const useCreateSell = (carId: string) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationKey: ['createSell', carId],
+        mutationFn: async (createSellParams: CreateSell) => {
+            const response = await createSell(createSellParams);
+
+            if (response?.status !== 201) {
+                throw new Error();
+            }
+
+            return response?.data;
+        },
+        onSuccess: (newSell) => {
+            queryClient.setQueryData(['sells'], (oldData: SellType[]) => {
+                return { ...oldData, newSell };
+            });
+        },
+    });
+};
