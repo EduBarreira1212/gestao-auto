bce783e6928467dc5ab1513cb2c8382b7098b489
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-21 12:36:19 -0300

    fix: ensure full response object is returned

--- web/src/services/car/getCarById.ts
+++ web/src/services/car/getCarById.ts
@@ -4,7 +4,7 @@ const getCarById = async (carId: string) => {
     try {
         const response = await axios.get(`http://localhost:3000/api/cars/${carId}`);
 
-        return response.data;
+        return response;
     } catch (error) {
         console.error('Error fetching data:', error);
     }
