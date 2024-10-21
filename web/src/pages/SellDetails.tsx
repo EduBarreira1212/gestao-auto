f25c61c48cfdb2d7fb2fb58281d4d66c2447765a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-21 12:34:16 -0300

    refactor: remove unused file

--- web/src/pages/SellDetails.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const SellDetails = () => {
-    const { sellId } = useParams();
-
-    return <div>{sellId}</div>;
-};
-
-export default SellDetails;
