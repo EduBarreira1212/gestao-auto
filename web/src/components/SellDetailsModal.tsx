95080a1e52761572204812666902b9bce0f38254
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-21 19:11:00 -0300

    feat: create sell details modal

---
+++ web/src/components/SellDetailsModal.tsx
@@ -0,0 +1,64 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { UpdateSell } from '../types';
+import { updateSellSchema } from '../schemas/zodSchemas';
+import { useUpdateSell } from '../hooks/data/updateSell';
+
+type SellDetailsModalProps = {
+    onClose: () => void;
+    sellId: string;
+};
+
+const SellDetailsModal = ({ onClose, sellId }: SellDetailsModalProps) => {
+    const { mutate } = useUpdateSell(sellId);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<UpdateSell>({
+        resolver: zodResolver(updateSellSchema),
+    });
+
+    const onSubmit: SubmitHandler<UpdateSell> = async (updateSellParams) => {
+        mutate(updateSellParams, {
+            onSuccess: () => {
+                onClose();
+            },
+        });
+    };
+
+    return (
+        <div className="fixed top-0 flex h-screen w-screen flex-col items-center justify-center backdrop-blur-sm">
+            <div className="w-80 bg-white p-5">
+                <button onClick={onClose}>X</button>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="flex flex-col gap-1 p-5"
+                >
+                    <label>Valor da venda:</label>
+                    <input
+                        className="border-2 p-2"
+                        type="number"
+                        {...register('amount', { valueAsNumber: true })}
+                    />
+                    {errors.amount && <p>{errors.amount.message}</p>}
+                    <label>Lucro:</label>
+                    <input
+                        className="border-2 p-2"
+                        type="number"
+                        {...register('profit', { valueAsNumber: true })}
+                    />
+                    {errors.profit && <p>{errors.profit.message}</p>}
+                    <input
+                        className="cursor-pointer border-2 bg-brand-secondary p-2 text-brand-primary hover:text-brand-accent"
+                        type="submit"
+                        value="Atualizar venda"
+                    />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default SellDetailsModal;
