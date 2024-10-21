6400e79eb8a7ffb844c3cc5e2b9d18515d9b632b
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-21 19:11:31 -0300

    feat: create logic to show modal and format values

--- web/src/components/Sell.tsx
+++ web/src/components/Sell.tsx
@@ -1,17 +1,41 @@
-import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 import { SellType } from '../types';
+import { useGetVehicleById } from '../hooks/data/getVehicleById';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import SellDetailsModal from './SellDetailsModal';
 
 const Sell = ({ sell }: { sell: SellType }) => {
-    const navigate = useNavigate();
+    const [showSellDetailsModal, setShowSelldetailsModal] = useState(false);
+
+    const { data: vehicle } = useGetVehicleById(sell.car_id);
+
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
 
     return (
-        <div className="flex w-fit flex-col gap-3 rounded-md border-2 border-solid bg-slate-50 p-5 text-center text-brand-secondary shadow-md shadow-brand-primary">
-            <span>Car: {sell.car_id}</span>
-            <span>Total: {sell.amount}</span>
-            <span>Profit: {sell.profit}</span>
-            <span>Data da venda: {sell.createdAt}</span>
-            <Button onClick={() => navigate('/')}>Ver detalhes</Button>
+        <div className="flex h-fit w-72 flex-col gap-3 rounded-md border-2 border-solid bg-slate-50 p-5 text-center text-brand-secondary shadow-md shadow-brand-primary">
+            <span>
+                Vehicle: {vehicle?.name} {vehicle?.year} {vehicle?.plate}
+            </span>
+            <span>Total: {formatter.format(sell.amount)}</span>
+            <span>Lucro: {formatter.format(sell.profit)}</span>
+            <span>
+                Data da venda: {new Date(sell.createdAt).toLocaleDateString('pt-BR')}
+            </span>
+            <Button onClick={() => setShowSelldetailsModal(true)}>
+                Ver detalhes
+            </Button>
+            {showSellDetailsModal &&
+                createPortal(
+                    <SellDetailsModal
+                        onClose={() => setShowSelldetailsModal(false)}
+                        sellId={sell.id}
+                    />,
+                    document.body
+                )}
         </div>
     );
 };
