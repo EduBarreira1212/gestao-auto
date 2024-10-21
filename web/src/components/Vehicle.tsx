eb4e9c7ee42855148b853b348c756ec347f2d13a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-21 12:37:29 -0300

    feat: add formatation on date and value properties

--- web/src/components/Vehicle.tsx
+++ web/src/components/Vehicle.tsx
@@ -14,7 +14,7 @@ type ICar = {
         plate: string;
         entry_price: number;
         expenses: [];
-        createdAt: string;
+        createdAt: Date;
     };
 };
 
@@ -22,15 +22,23 @@ const Vehicle = ({ car }: ICar) => {
     const [showCreateSellModal, setShowCreateSellModal] = useState(false);
     const navigate = useNavigate();
 
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
     return (
         <div className="flex w-fit flex-col gap-3 rounded-md border-2 border-solid bg-slate-50 p-5 text-center text-brand-secondary shadow-md shadow-brand-primary">
             <span>{car.name}</span>
             <span>{car.brand}</span>
             <span>{car.year}</span>
             <span>{car.plate}</span>
-            <span>{car.entry_price}</span>
-            <span>{car.expenses}</span>
-            <span>{car.createdAt}</span>
+            <span>Preço de entrada: {formatter.format(car.entry_price)}</span>
+            <span>Despesas: {car.expenses}</span>
+            <span>
+                Data de entrada:{' '}
+                {new Date(car.createdAt).toLocaleDateString('pt-BR')}
+            </span>
             <div className="flex flex-row justify-between">
                 <Button onClick={() => navigate('/')}>Ver detalhes</Button>
                 <Button onClick={() => setShowCreateSellModal(true)}>Vender</Button>
