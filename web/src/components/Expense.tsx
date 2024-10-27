eb524a2ffe41df0eb22cc821df7cf3bb35440aff
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-26 21:18:22 -0300

    feat: create logic to display the vehicle info and format amount

--- web/src/components/Expense.tsx
+++ web/src/components/Expense.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import Button from './Button';
+import { useGetVehicleById } from '../hooks/data/useGetVehicleById';
 
 type ExpenseProps = {
     expense: {
@@ -14,12 +15,24 @@ type ExpenseProps = {
 const Expense = ({ expense }: ExpenseProps) => {
     const navigate = useNavigate();
 
+    const { data: vehicle } = useGetVehicleById(expense.car_id ?? '');
+
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
     return (
         <div className="flex w-fit flex-col gap-3 rounded-md border-2 border-solid bg-slate-50 p-5 text-center text-brand-secondary shadow-md shadow-brand-primary">
-            <span>{expense.car_id}</span>
-            <span>{expense.amount}</span>
+            <span>
+                Veículo: {vehicle?.name} {vehicle?.plate} {vehicle?.year}
+            </span>
+            <span>{formatter.format(expense.amount)}</span>
             <span>{expense.description}</span>
-            <span>Data da despesa: 0</span>
+            <span>
+                Data da despesa:{' '}
+                {new Date(expense.createdAt).toLocaleDateString('pt-BR')}
+            </span>
             <Button onClick={() => navigate('/')}>Ver detalhes</Button>
         </div>
     );
