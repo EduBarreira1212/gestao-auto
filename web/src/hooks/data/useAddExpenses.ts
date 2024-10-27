c5ca68dde2a31c2c425255ac64463a442c70330a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-27 20:08:24 -0300

    feat: add function on sucess to update the cache with the new expenses

--- web/src/hooks/data/useAddExpenses.ts
+++ web/src/hooks/data/useAddExpenses.ts
@@ -1,8 +1,11 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import createExpense from '../../services/expense/createExpense';
-import { CreateExpense } from '../../types';
+import { CreateExpense, VehicleType } from '../../types';
+import { vehicleQueriesKeys } from '../../keys/queries';
 
 export const useAddExpenses = () => {
+    const queryClient = useQueryClient();
+
     return useMutation({
         mutationKey: ['addExpense'],
         mutationFn: async (createExpenseParams: CreateExpense) => {
@@ -13,6 +16,29 @@ export const useAddExpenses = () => {
             }
 
             return response.data;
+        },
+        onSuccess: (newExpense) => {
+            queryClient.setQueryData(
+                vehicleQueriesKeys.getVehicles(),
+                (oldData: VehicleType[]) => {
+                    const indexOfVehicle = oldData.findIndex((vehicle) => {
+                        return vehicle.id === newExpense.car_id;
+                    });
+
+                    if (indexOfVehicle === -1) return oldData;
+
+                    const updatedVehicle = {
+                        ...oldData[indexOfVehicle],
+                        expenses: [...oldData[indexOfVehicle].expenses, newExpense],
+                    };
+
+                    return [
+                        ...oldData.slice(0, indexOfVehicle),
+                        updatedVehicle,
+                        ...oldData.slice(indexOfVehicle + 1),
+                    ];
+                }
+            );
         },
     });
 };
