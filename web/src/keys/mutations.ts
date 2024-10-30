476870965548a514671b3b519ccd26983f47e83d
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-30 19:10:38 -0300

    feat: create expense mutation keys

--- web/src/keys/mutations.ts
+++ web/src/keys/mutations.ts
@@ -6,6 +6,12 @@ export const sellMutationsKeys = {
 
 export const vehicleMutationsKeys = {
     addVehicle: () => ['addVehicle'],
-    deleteVehicle: (vehicleId: string) => ['deleteSell', vehicleId],
+    deleteVehicle: (vehicleId: string) => ['deleteVehicle', vehicleId],
     updateVehicle: (vehicleId: string) => ['updateVehicle', vehicleId],
+};
+
+export const expenseMutationsKeys = {
+    addExpense: () => ['addExpense'],
+    deleteExpense: (expenseId: string) => ['deleteExpense', expenseId],
+    updateExpense: (expenseId: string) => ['updateExpense', expenseId],
 };
