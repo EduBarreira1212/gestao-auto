be677fe4bbef9c1caef99d337ee569a9777456b4
Eduardo Barreira <edubarreira1212@gmail.com>
2024-10-31 12:23:43 -0300

    feat: create deleteExpenseModal

---
+++ web/src/components/DeleteExpenseModal.tsx
@@ -0,0 +1,42 @@
+import { useDeleteExpense } from '../hooks/data/useDeleteExpense';
+import ModalContainer from './ModalContainer';
+
+type DeleteExpenseModalProps = {
+    expenseId: string;
+    onClose: () => void;
+};
+
+const DeleteExpenseModal = ({ expenseId, onClose }: DeleteExpenseModalProps) => {
+    const { mutate } = useDeleteExpense(expenseId);
+
+    return (
+        <ModalContainer>
+            <button onClick={onClose}>X</button>
+            <div className="flex flex-col gap-3">
+                <h2>Tem certeza que deseja excluir essa despesa?</h2>
+                <div className="flex flex-row justify-evenly">
+                    <button
+                        className="my-3 self-center rounded-sm border-2 border-solid bg-slate-300 p-2 text-black"
+                        onClick={onClose}
+                    >
+                        Cancelar
+                    </button>
+                    <button
+                        className="my-3 self-center rounded-sm border-2 border-solid bg-red-600 p-2 text-brand-neutral"
+                        onClick={() => {
+                            mutate(undefined, {
+                                onSuccess: () => {
+                                    onClose();
+                                },
+                            });
+                        }}
+                    >
+                        Excluir
+                    </button>
+                </div>
+            </div>
+        </ModalContainer>
+    );
+};
+
+export default DeleteExpenseModal;
