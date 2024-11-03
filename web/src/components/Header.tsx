49270e6370c53b93a74833697cfdca65c8ca46cb
Eduardo Barreira <edubarreira1212@gmail.com>
2024-11-02 21:06:16 -0300

    feat: create logic to open and close navbar modal

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,14 +1,22 @@
 import { Link, useLocation } from 'react-router-dom';
 import { UserButton, useUser } from '@clerk/clerk-react';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import NavbarModal from '../modals/NavbarModal';
 
 const Header = () => {
+    const [showNavbarModal, setShowNavbarModal] = useState(false);
+
     const { user } = useUser();
 
     const location = useLocation();
 
     return (
         <header className="flex w-full flex-row items-center justify-between bg-brand-secondary px-8 py-3 text-brand-primary shadow-sm shadow-black">
-            <button className="rounded-lg border-2 border-black px-1 pb-0.5 text-2xl shadow-lg shadow-black md:hidden">
+            <button
+                className="rounded-lg border-2 border-black px-1 pb-0.5 text-2xl shadow-lg shadow-black md:hidden"
+                onClick={() => setShowNavbarModal(true)}
+            >
                 {'>'}
             </button>
             <h2 className="md:hidden">
@@ -33,6 +41,11 @@ const Header = () => {
                 <UserButton />
                 <span>{user?.fullName}</span>
             </div>
+            {showNavbarModal &&
+                createPortal(
+                    <NavbarModal onClose={() => setShowNavbarModal(false)} />,
+                    document.body
+                )}
         </header>
     );
 };
