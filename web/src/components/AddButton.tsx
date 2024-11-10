b0a1ffe452c63253dedaf2cc1cbc19ef47540f25
Eduardo Barreira <edubarreira1212@gmail.com>
2024-11-10 20:24:12 -0300

    feat: add transition in hover effect

--- web/src/components/AddButton.tsx
+++ web/src/components/AddButton.tsx
@@ -7,7 +7,7 @@ const AddButton = ({ children, onClick }: AddButtonProps) => {
     return (
         <button
             onClick={onClick}
-            className="mt-2 rounded-md border-2 bg-brand-primary p-2 text-brand-neutral hover:text-brand-accent"
+            className="mt-2 rounded-md border-2 bg-brand-primary p-2 text-brand-neutral transition-colors duration-200 hover:bg-[#009297] hover:text-brand-accent"
         >
             {children}
         </button>
