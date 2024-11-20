62256597142affc49e1cd7c44b33f1bd5bdc2295
Eduardo Barreira <edubarreira1212@gmail.com>
2024-11-20 18:11:07 -0300

    feat: add km in car fixture

--- server/src/tests/fixtures/car.js
+++ server/src/tests/fixtures/car.js
@@ -7,5 +7,6 @@ export const carFixture = {
     name: faker.vehicle.model(),
     year: 2018,
     plate: 'AAA0A00',
+    km: faker.number.int({ min: 0, max: 200000 }),
     entry_price: Number(faker.commerce.price({ min: 20000, max: 1000000 })),
 };
