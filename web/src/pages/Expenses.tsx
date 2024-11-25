ec0ad77dbcc226d2ea41f57e3317fb24625ed16a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-11-25 19:16:31 -0300

    feat: add loading spinner in expenses component

--- web/src/pages/Expenses.tsx
+++ web/src/pages/Expenses.tsx
@@ -1,3 +1,4 @@
+import loading from '../assets/loading.png';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Expense from '../components/Expense';
@@ -11,7 +12,7 @@ import Screen from '../components/Screen';
 const Expenses = () => {
     const { user } = useUser();
 
-    const { data: vehicles } = useGetVehicles(user?.externalId ?? '');
+    const { data: vehicles, isLoading } = useGetVehicles(user?.externalId ?? '');
 
     const expenses = vehicles?.flatMap((vehicle: VehicleType) => vehicle.expenses);
 
@@ -21,7 +22,15 @@ const Expenses = () => {
             <div className="flex w-full flex-col bg-brand-neutral">
                 <Header />
                 <ContentSection>
-                    {expenses?.length > 0 ? (
+                    {isLoading ? (
+                        <div className="flex h-full w-full items-center justify-center">
+                            <img
+                                src={loading}
+                                alt="loading"
+                                className="size-12 animate-spin md:size-14"
+                            />
+                        </div>
+                    ) : expenses?.length > 0 ? (
                         <List>
                             {expenses.map((expense: ExpenseType) => (
                                 <li key={expense.id}>
