f48b200010b270ce86f15b8faa3e0573112cda83
Eduardo Barreira <edubarreira1212@gmail.com>
2024-11-28 11:33:36 -0300

    feat: add font in button component

--- web/src/components/SubmitBtn.tsx
+++ web/src/components/SubmitBtn.tsx
@@ -9,7 +9,7 @@ const SubmitBtn = ({ value, disabled }: SubmitBtnPropsType) => {
             type="submit"
             value={value}
             disabled={disabled}
-            className="cursor-pointer border-2 bg-brand-secondary p-2 text-brand-neutral transition-colors duration-200 hover:bg-[#070a1d] hover:text-brand-accent"
+            className="font-montserrat cursor-pointer border-2 bg-brand-secondary p-2 text-brand-neutral transition-colors duration-200 hover:bg-[#070a1d] hover:text-brand-accent"
         />
     );
 };
