6b8e1874a9543ef670e326935fb1b29835708640
Eduardo Barreira <edubarreira1212@gmail.com>
2024-11-30 20:16:11 -0300

    fix: ensure div with page name and div with Links are centralized

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -12,18 +12,18 @@ const Header = () => {
     const location = useLocation();
 
     return (
-        <header className="flex w-full flex-row items-center justify-between bg-brand-secondary px-5 py-3 text-brand-primary shadow-sm shadow-black">
+        <header className="relative flex w-full flex-row items-center justify-between bg-brand-secondary px-5 py-3 text-brand-primary shadow-sm shadow-black">
             <button
                 className="rounded-lg border-2 border-black px-1 pb-0.5 text-2xl shadow-lg shadow-black md:hidden"
                 onClick={() => setShowNavbarModal(true)}
             >
                 {'>'}
             </button>
-            <h2 className="md:hidden">
+            <h2 className="absolute left-1/2 -translate-x-1/2 transform md:hidden">
                 {location.pathname.slice(1).charAt(0).toUpperCase() +
                     location.pathname.slice(2)}
             </h2>
-            <div className="mx-auto hidden gap-4 md:flex">
+            <div className="absolute left-1/2 hidden -translate-x-1/2 transform gap-4 md:flex">
                 <Link
                     className="font-poppins transition-all duration-200 hover:scale-110 hover:text-brand-accent"
                     to="/vendas"
@@ -49,9 +49,11 @@ const Header = () => {
                     Leads
                 </Link>
             </div>
-            <div className="flex items-center gap-2">
+            <div className="ml-auto flex items-center gap-2">
                 <UserButton />
-                <span className="font-montserrat">{user?.fullName}</span>
+                <span className="h-6 w-[4.5rem] overflow-hidden font-montserrat">
+                    {user?.fullName}
+                </span>
             </div>
             {showNavbarModal &&
                 createPortal(
