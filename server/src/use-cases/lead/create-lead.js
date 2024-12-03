3b3c5f91dcea821553b4dd67e33cb4e86a81d672
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-03 19:19:58 -0300

    feat: create use case to create a lead

---
+++ server/src/use-cases/lead/create-lead.js
@@ -0,0 +1,55 @@
+export class CreateLeadUseCase {
+    constructor(
+        postgresGetUserByIdRepository,
+        postgresGetLeadByEmailRepository,
+        postgresGetLeadByPhoneRepository,
+        idGeneratorAdapter,
+        postgresCreateLeadRepository
+    ) {
+        this.postgresGetUserByIdRepository = postgresGetUserByIdRepository;
+        this.postgresGetLeadByEmailRepository = postgresGetLeadByEmailRepository;
+        this.postgresGetLeadByPhoneRepository = postgresGetLeadByPhoneRepository;
+        this.idGeneratorAdapter = idGeneratorAdapter;
+        this.postgresCreateLeadRepository = postgresCreateLeadRepository;
+    }
+
+    async execute(createLeadParams) {
+        const userId = createLeadParams.user_id;
+
+        const userWithProvidedIdExists =
+            await this.postgresGetUserByIdRepository.execute(userId);
+
+        if (!userWithProvidedIdExists) {
+            throw new Error('User with ID not found');
+        }
+
+        const leadEmail = createLeadParams.email;
+
+        const leadWithProvidedEmailExists =
+            await this.postgresGetLeadByEmailRepository.execute(leadEmail);
+
+        if (!leadWithProvidedEmailExists) {
+            throw new Error('E-mail already in use by other lead');
+        }
+
+        const leadPhone = createLeadParams.phone;
+
+        const leadWithProvidedPhoneExists =
+            await this.postgresGetLeadByPhoneRepository.execute(leadPhone);
+
+        if (!leadWithProvidedPhoneExists) {
+            throw new Error('Phone number already in use by other lead');
+        }
+
+        const leadId = await this.idGeneratorAdapter.execute();
+
+        const lead = {
+            ...createLeadParams,
+            id: leadId,
+        };
+
+        const createdLead = await this.postgresCreateLeadRepository.execute(lead);
+
+        return createdLead;
+    }
+}
