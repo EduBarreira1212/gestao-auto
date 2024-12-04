c7ec46ab812b7f72cb446dc73b61e8e065eea7f9
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-04 16:36:09 -0300

    feat: create the controller to get a lead by id

---
+++ server/src/controllers/lead/get-lead-by-id.js
@@ -0,0 +1,30 @@
+import { validate } from 'uuid';
+
+export class GetLeadByIdController {
+    constructor(getLeadByIdUseCase) {
+        this.getLeadByIdUseCase = getLeadByIdUseCase;
+    }
+
+    async execute(httpRequest) {
+        try {
+            const leadId = httpRequest.params.leadId;
+
+            const isIdValid = validate(leadId);
+
+            if (!isIdValid) {
+                return { statusCode: 400, body: { message: 'ID invalid' } };
+            }
+
+            const lead = await this.getLeadByIdUseCase(leadId);
+
+            if (!lead) {
+                return { statusCode: 404, body: { message: 'Lead not found' } };
+            }
+
+            return { statusCode: 200, body: lead };
+        } catch (error) {
+            console.error(error);
+            return { statusCode: 500, body: { error } };
+        }
+    }
+}
