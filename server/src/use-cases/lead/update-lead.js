e3b6e433e00b81c5b181b36f5f2358624f80bfe3
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-05 21:09:04 -0300

    feat: add email and phone already in use validation

--- server/src/use-cases/lead/update-lead.js
+++ server/src/use-cases/lead/update-lead.js
@@ -1,6 +1,13 @@
 export class UpdateLeadUseCase {
-    constructor(postgresGetLeadByIdRepository, postgresUpdateLeadRepository) {
+    constructor(
+        postgresGetLeadByIdRepository,
+        postgresGetLeadByEmailRepository,
+        postgresGetLeadByPhoneRepository,
+        postgresUpdateLeadRepository
+    ) {
         this.postgresGetLeadByIdRepository = postgresGetLeadByIdRepository;
+        this.postgresGetLeadByEmailRepository = postgresGetLeadByEmailRepository;
+        this.postgresGetLeadByPhoneRepository = postgresGetLeadByPhoneRepository;
         this.postgresUpdateLeadRepository = postgresUpdateLeadRepository;
     }
     async execute(leadId, updateLeadParams) {
@@ -9,6 +16,33 @@ export class UpdateLeadUseCase {
 
         if (!leadWithProvidedIdExists) {
             throw new Error('Lead not found');
+        }
+
+        if (updateLeadParams.email) {
+            const leadWithProvidedEmailExists =
+                await this.postgresGetLeadByEmailRepository.execute(
+                    updateLeadParams.email
+                );
+
+            if (
+                leadWithProvidedEmailExists &&
+                leadWithProvidedEmailExists.id !== leadId
+            ) {
+                throw new Error('E-mail already in use');
+            }
+        }
+
+        if (updateLeadParams.phone) {
+            const leadWithProvidedPhoneExists =
+                await this.postgresGetLeadByPhoneRepository.execute(
+                    updateLeadParams.phone
+                );
+            if (
+                leadWithProvidedPhoneExists &&
+                leadWithProvidedPhoneExists.id !== leadId
+            ) {
+                throw new Error('Phone number already in use');
+            }
         }
 
         const updatedLead = await this.postgresUpdateLeadRepository.execute(
