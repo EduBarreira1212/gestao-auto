0566fbb2ed69a835e81c120c13693aedf08e9bca
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-09 11:17:49 -0300

    feat: create sell fixture

--- server/src/tests/fixtures/sell.js
+++ server/src/tests/fixtures/sell.js
@@ -4,6 +4,7 @@ export const sellFixture = {
     id: faker.string.uuid(),
     user_id: faker.string.uuid(),
     car_id: faker.string.uuid(),
+    lead_id: faker.string.uuid(),
     amount: Number(faker.commerce.price({ min: 20000, max: 1000000 })),
     profit: Number(faker.commerce.price({ min: 1000, max: 100000 })),
 };
