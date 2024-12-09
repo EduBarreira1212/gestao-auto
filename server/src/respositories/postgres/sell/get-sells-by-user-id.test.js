6e4a413818ef673b4dfecfb132ec4dc3a780c40a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-09 11:20:08 -0300

    fix: ensure get sells by user id repository create a lead and return object with lead id

--- server/src/respositories/postgres/sell/get-sells-by-user-id.test.js
+++ server/src/respositories/postgres/sell/get-sells-by-user-id.test.js
@@ -2,6 +2,7 @@ import { PostgresGetSellsByUserIddRepository } from './get-sells-by-user-id.js';
 import { userFixture as user } from '../../../tests/fixtures/user.js';
 import { carFixture as car } from '../../../tests/fixtures/car.js';
 import { sellFixture as sell } from '../../../tests/fixtures/sell.js';
+import { leadFixture as lead } from '../../../tests/fixtures/lead.js';
 
 import prisma from '../../../../prisma/prisma.js';
 
@@ -11,8 +12,9 @@ describe('PostgresGetSellsByUserIdRepository', () => {
     test('should get sells sucessfully', async () => {
         await prisma.user.create({ data: user });
         await prisma.car.create({ data: { ...car, user_id: user.id } });
+        await prisma.lead.create({ data: { ...lead, user_id: user.id } });
         await prisma.sell.create({
-            data: { ...sell, user_id: user.id, car_id: car.id },
+            data: { ...sell, user_id: user.id, car_id: car.id, lead_id: lead.id },
         });
 
         const result = await sut.execute(user.id);
@@ -23,8 +25,9 @@ describe('PostgresGetSellsByUserIdRepository', () => {
     test('should return a array of sells with correct properties', async () => {
         await prisma.user.create({ data: user });
         await prisma.car.create({ data: { ...car, user_id: user.id } });
+        await prisma.lead.create({ data: { ...lead, user_id: user.id } });
         await prisma.sell.create({
-            data: { ...sell, user_id: user.id, car_id: car.id },
+            data: { ...sell, user_id: user.id, car_id: car.id, lead_id: lead.id },
         });
 
         const result = await sut.execute(user.id);
@@ -34,6 +37,7 @@ describe('PostgresGetSellsByUserIdRepository', () => {
                 id: sell.id,
                 user_id: user.id,
                 car_id: car.id,
+                lead_id: lead.id,
                 amount: sell.amount,
                 profit: sell.profit,
                 createdAt: result[0].createdAt,
