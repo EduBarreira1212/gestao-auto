5145d17b6d72125f8676e6759825b21302f0e90f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-10 18:50:02 -0300

    feat: create logic to fetch and display leads

--- web/src/pages/Leads.tsx
+++ web/src/pages/Leads.tsx
@@ -1,10 +1,20 @@
+import { useUser } from '@clerk/clerk-react';
+import loading from '../assets/icons/loading.png';
 import AddButton from '../components/AddButton';
 import ContentSection from '../components/ContentSection';
 import Header from '../components/Header';
+import Lead from '../components/Lead';
 import Navbar from '../components/Navbar';
 import Screen from '../components/Screen';
+import { useGetLeads } from '../hooks/data/useGetLeads';
+import List from '../components/List';
+import { LeadType } from '../types';
 
 const Leads = () => {
+    const { user } = useUser();
+
+    const { data: leads, isLoading } = useGetLeads(user?.externalId ?? '');
+
     return (
         <Screen>
             <Navbar />
@@ -14,7 +24,25 @@ const Leads = () => {
                     <AddButton onClick={() => console.log('Lead')}>
                         Adicionar novo lead
                     </AddButton>
-                    <div>Nenhum lead encontrado</div>
+                    {isLoading ? (
+                        <div className="flex h-full w-full items-center justify-center">
+                            <img
+                                src={loading}
+                                alt="loading"
+                                className="size-12 animate-spin md:size-14"
+                            />
+                        </div>
+                    ) : Array.isArray(leads) && leads?.length > 0 ? (
+                        <List>
+                            {leads.map((lead: LeadType) => (
+                                <li key={lead.id}>
+                                    <Lead lead={lead} />
+                                </li>
+                            ))}
+                        </List>
+                    ) : (
+                        <div>Nenhum lead encontrado</div>
+                    )}
                 </ContentSection>
             </div>
         </Screen>
