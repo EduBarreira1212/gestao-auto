e09ba17c15e44fcda54e498ed4f400af3b2dbec9
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-14 20:40:42 -0300

    feat: include purchases when returning the leads

--- server/src/respositories/postgres/lead/get-leads-by-user-id.js
+++ server/src/respositories/postgres/lead/get-leads-by-user-id.js
@@ -2,7 +2,10 @@ import prisma from '../../../../prisma/prisma.js';
 
 export class PostgresGetLeadsByUserIdRepository {
     async execute(userId) {
-        const leads = await prisma.lead.findMany({ where: { user_id: userId } });
+        const leads = await prisma.lead.findMany({
+            where: { user_id: userId },
+            include: { purchases: true },
+        });
 
         return leads;
     }
