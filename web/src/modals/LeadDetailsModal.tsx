885745e430ecdeb50a5a66f9be08d9d159648302
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-14 20:42:29 -0300

    feat: create modal to update a lead

---
+++ web/src/modals/LeadDetailsModal.tsx
@@ -0,0 +1,77 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { LeadType, UpdateLead } from '../types';
+import { updateLeadSchema } from '../schemas/zodSchemas';
+import ModalContainer from '../components/ModalContainer';
+import SubmitBtn from '../components/SubmitBtn';
+import InputErrorMessage from '../components/InputErrorMessage';
+import { useUpdateLead } from '../hooks/data/useUpdateLead';
+
+type LeadDetailsModalProps = {
+    onClose: () => void;
+    lead: LeadType;
+};
+
+const LeadDetailsModal = ({ onClose, lead }: LeadDetailsModalProps) => {
+    const { mutate, isPending } = useUpdateLead(lead.id);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<UpdateLead>({
+        resolver: zodResolver(updateLeadSchema),
+        defaultValues: {
+            name: lead.name,
+            email: lead.email,
+            phone: lead.phone,
+            birthday: lead.birthday.toString(),
+        },
+    });
+
+    const onSubmit: SubmitHandler<UpdateLead> = async (updateLeadParams) => {
+        mutate(updateLeadParams, {
+            onSuccess: () => {
+                onClose();
+            },
+        });
+    };
+
+    return (
+        <ModalContainer>
+            <button onClick={onClose}>X</button>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col gap-1 p-5"
+            >
+                <label htmlFor="">Nome:</label>
+                <input className="border-2 p-2" type="text" {...register('name')} />
+                {errors.name && (
+                    <InputErrorMessage>{errors.name.message}</InputErrorMessage>
+                )}
+                <label htmlFor="">E-mail:</label>
+                <input className="border-2 p-2" type="text" {...register('email')} />
+                {errors.email && (
+                    <InputErrorMessage>{errors.email.message}</InputErrorMessage>
+                )}
+                <label htmlFor="">Telefone:</label>
+                <input className="border-2 p-2" type="text" {...register('phone')} />
+                {errors.phone && (
+                    <InputErrorMessage>{errors.phone.message}</InputErrorMessage>
+                )}
+                <label htmlFor="">Data de nascimento:</label>
+                <input
+                    className="border-2 p-2"
+                    type="date"
+                    {...register('birthday')}
+                />
+                {errors.birthday && (
+                    <InputErrorMessage>{errors.birthday.message}</InputErrorMessage>
+                )}
+                <SubmitBtn value="Atualizar" disabled={isPending} />
+            </form>
+        </ModalContainer>
+    );
+};
+
+export default LeadDetailsModal;
