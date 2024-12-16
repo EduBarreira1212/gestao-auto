2e94451ac94b11c7192746b298ef23a81e75c0ed
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-16 11:43:14 -0300

    feat: add style to button

--- web/src/components/CloseModalBtn.tsx
+++ web/src/components/CloseModalBtn.tsx
@@ -1,6 +1,9 @@
 const CloseModalBtn = ({ onClick }: { onClick: () => void }) => {
     return (
-        <button className="border-2" onClick={onClick}>
+        <button
+            className="rounded-sm border-2 px-2 text-brand-secondary transition-colors duration-200 hover:bg-brand-neutral hover:text-brand-accent"
+            onClick={onClick}
+        >
             X
         </button>
     );
