d22a66e5afeb4a26675dd8b86c7c8a9a4d138bea
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-16 11:42:49 -0300

    refactor: change order of classes

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({ onClick, children }: ButtonProps) => {
     return (
         <button
             onClick={onClick}
-            className="font-montserrat my-3 self-center rounded-sm border-2 border-solid bg-brand-secondary p-2 text-brand-neutral transition-colors duration-200 hover:bg-[#070a1d] hover:text-brand-accent"
+            className="my-3 self-center rounded-sm border-2 border-solid bg-brand-secondary p-2 font-montserrat text-brand-neutral transition-colors duration-200 hover:bg-[#070a1d] hover:text-brand-accent"
         >
             {children}
         </button>
