c6cb666f7df87c7a131e02b3e0122483fb1c0c3b
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-18 22:43:01 -0300

    chore: add Elements from stripe in components tree

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,17 +2,23 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App.tsx';
 import './index.css';
 
 const queryClient = new QueryClient();
 
+const stripePromise = loadStripe(process.env.VITE_STRIPE_PUBLIC_KEY);
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
-                <App />
+                <Elements stripe={stripePromise}>
+                    <App />
+                </Elements>
             </BrowserRouter>
         </QueryClientProvider>
     </StrictMode>
