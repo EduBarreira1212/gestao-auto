d60d51e349e859fef5305c26e0be2ca285226faa
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-19 12:49:57 -0300

    refactor: get price id in req.body and change sucess and cancel url

--- server/src/routes/stripe.js
+++ server/src/routes/stripe.js
@@ -10,24 +10,20 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 stripeRouter.post('/create-checkout-session', async (req, res) => {
     try {
-        const prices = await stripe.prices.list({
-            lookup_keys: [req.body.lookup_key],
-            expand: ['data.product'],
-        });
         const session = await stripe.checkout.sessions.create({
             billing_address_collection: 'auto',
             line_items: [
                 {
-                    price: prices.data[0].id,
+                    price: req.body.priceId,
                     quantity: 1,
                 },
             ],
             mode: 'subscription',
-            success_url: `${process.env.FRONTEND_URL}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: `${process.env.FRONTEND_URL}?canceled=true`,
+            success_url: `${process.env.FRONTEND_URL}/sign-in`,
+            cancel_url: `${process.env.FRONTEND_URL}/cadastro`,
         });
 
-        res.redirect(303, session.url);
+        res.json({ url: session.url });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
