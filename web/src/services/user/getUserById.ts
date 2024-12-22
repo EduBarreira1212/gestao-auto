c9dc097bf6c8c3dc1f9ba9b8eecccf41e10fb660
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-22 19:46:42 -0300

    fix: ensure auth token is send and full response object is returned

--- web/src/services/user/getUserById.ts
+++ web/src/services/user/getUserById.ts
@@ -1,10 +1,12 @@
 import { api } from '../../lib/axios';
 
-const getUserById = async (userId: string) => {
+const getUserById = async (userId: string, token: string) => {
     try {
-        const response = await api.get(`/users/${userId}`);
+        const response = await api.get(`/users/${userId}`, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
 
-        return response.data;
+        return response;
     } catch (error) {
         console.error('Error fetching data:', error);
     }
