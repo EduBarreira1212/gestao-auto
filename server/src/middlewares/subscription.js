df731eb6315cb25e3568ba17ed28c03104ceb3f8
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-23 11:25:23 -0300

    feat: create the logic of subscription middleware

--- server/src/middlewares/subscription.js
+++ server/src/middlewares/subscription.js
@@ -1,5 +1,32 @@
-export const subscriptionMiddleware = (req, res, next) => {
-    console.log(req);
+import { ClerkClientAdapter } from '../adapters/clerk-client.js';
+import { makeGetUserByIdController } from '../factories/controllers/user.js';
+
+export const subscriptionMiddleware = async (req, res, next) => {
+    const clerkClientAdapter = new ClerkClientAdapter();
+
+    const user = await clerkClientAdapter.getUser(req.auth.userId);
+
+    const getUserById = makeGetUserByIdController();
+
+    const modifiedRequest = {
+        params: {
+            userId: user.externalId,
+        },
+    };
+
+    const { body } = await getUserById.execute(modifiedRequest);
+
+    if (!body) {
+        return res
+            .status(401)
+            .json({ error: 'Unauthorized. User not authenticated.' });
+    }
+
+    if (!body.paid) {
+        return res
+            .status(403)
+            .json({ error: 'Access denied. Subscription required.' });
+    }
 
     next();
 };
