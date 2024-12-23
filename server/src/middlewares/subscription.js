294222039ccc9670a34182aed3dcf98d1d4d7f0e
Eduardo Barreira <edubarreira1212@gmail.com>
2024-12-23 10:45:52 -0300

    refactor: log the request object

--- server/src/middlewares/subscription.js
+++ server/src/middlewares/subscription.js
@@ -1,19 +1,5 @@
 export const subscriptionMiddleware = (req, res, next) => {
     console.log(req);
 
-    const user = req.user;
-
-    if (!user) {
-        return res
-            .status(401)
-            .json({ error: 'Unauthorized. User not authenticated.' });
-    }
-
-    if (!user.paid) {
-        return res
-            .status(403)
-            .json({ error: 'Access denied. Subscription required.' });
-    }
-
     next();
 };
