d574d4e4051bc39e0125cf67eb077de88e51ae5f
Eduardo Barreira <edubarreira1212@gmail.com>
2025-01-17 12:22:16 -0300

    feat: create logic to filter the vehicles list and display

--- web/src/pages/Vehicles.tsx
+++ web/src/pages/Vehicles.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import loading from '../assets/icons/loading.png';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
@@ -14,11 +14,23 @@ import { useGetVehicles } from '../hooks/data/useGetVehicles';
 import Screen from '../components/Screen';
 
 const Vehicles = () => {
+    const [filter, setFilter] = useState('all');
     const [showAddVehicleModal, setShowAddVehicleModal] = useState(false);
 
     const { user } = useUser();
 
     const { data, isLoading } = useGetVehicles(user?.externalId ?? '');
+
+    const filteredData = useMemo(() => {
+        if (filter === 'all') return data;
+        if (filter === 'stock') {
+            return data.filter((vehicle: VehicleType) => vehicle.sell === null);
+        }
+        if (filter === 'sold') {
+            return data.filter((vehicle: VehicleType) => vehicle.sell !== null);
+        }
+        return [];
+    }, [filter, data]);
 
     return (
         <Screen>
@@ -26,9 +38,31 @@ const Vehicles = () => {
             <div className="flex w-full flex-col bg-brand-neutral">
                 <Header />
                 <ContentSection>
-                    <AddButton onClick={() => setShowAddVehicleModal(true)}>
-                        Adicionar novo veículo
-                    </AddButton>
+                    <div className="relative flex w-full items-center">
+                        <div className="flex gap-1">
+                            <button
+                                className={`rounded-md border-2 bg-brand-secondary p-1 text-brand-neutral ${filter === 'all' ? 'border-brand-primary text-brand-accent' : ''}`}
+                                onClick={() => setFilter('all')}
+                            >
+                                Todos
+                            </button>
+                            <button
+                                className={`rounded-md border-2 bg-brand-secondary p-1 text-brand-neutral ${filter === 'stock' ? 'border-brand-primary text-brand-accent' : ''}`}
+                                onClick={() => setFilter('stock')}
+                            >
+                                Estoque
+                            </button>
+                            <button
+                                className={`rounded-md border-2 bg-brand-secondary p-1 text-brand-neutral ${filter === 'sold' ? 'border-brand-primary text-brand-accent' : ''}`}
+                                onClick={() => setFilter('sold')}
+                            >
+                                Vendidos
+                            </button>
+                        </div>
+                        <AddButton onClick={() => setShowAddVehicleModal(true)}>
+                            Adicionar novo veículo
+                        </AddButton>
+                    </div>
                     {isLoading ? (
                         <div className="flex h-full w-full items-center justify-center">
                             <img
@@ -37,9 +71,9 @@ const Vehicles = () => {
                                 className="size-12 animate-spin md:size-14"
                             />
                         </div>
-                    ) : Array.isArray(data) && data?.length > 0 ? (
+                    ) : Array.isArray(filteredData) && filteredData?.length > 0 ? (
                         <List>
-                            {data.map((vehicle: VehicleType) => (
+                            {filteredData.map((vehicle: VehicleType) => (
                                 <li key={vehicle.id}>
                                     <Vehicle car={vehicle} />
                                 </li>
