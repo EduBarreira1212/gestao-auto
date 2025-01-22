925d9381b4be5038c6734f09edc3ed20f2e65d3f
Eduardo Barreira <edubarreira1212@gmail.com>
2025-01-22 19:27:40 -0300

    feat: create logic to send photos to AWS S3

--- server/src/use-cases/car/create-car.js
+++ server/src/use-cases/car/create-car.js
@@ -1,3 +1,6 @@
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import s3Client from '../../../aws-S3/aws-s3.js';
+
 export class CreateCarUseCase {
     constructor(
         postgresGetUserByIdRepository,
@@ -19,10 +22,33 @@ export class CreateCarUseCase {
 
         const carId = this.idGeneratorAdapter.execute();
 
+        const { photos, ...paramsWithoutPhotos } = createCarParams;
+
         const car = {
-            ...createCarParams,
+            ...paramsWithoutPhotos,
             id: carId,
+            photoUrls: [],
         };
+
+        if (photos) {
+            const uploadPromises = photos.map(async (file, index) => {
+                const command = new PutObjectCommand({
+                    Bucket: process.env.AWS_BUCKET_NAME,
+                    ACL: 'public-read',
+                    Key: `photos/${carId}-${index}.${file.mimetype.split('/')[1]}`,
+                    Body: file.buffer,
+                    ContentType: file.mimetype,
+                });
+
+                await s3Client.send(command);
+
+                const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/photos/${carId}-${index}.${file.mimetype.split('/')[1]}`;
+
+                car.photoUrls.push(fileUrl);
+            });
+
+            await Promise.all(uploadPromises);
+        }
 
         const carCreated = await this.postgresCreateCarRepository.execute(car);
 
