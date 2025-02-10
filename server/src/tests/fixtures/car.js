ab9eec33418e33d8f23bafa29f9a17cf60430d96
Eduardo Barreira <edubarreira1212@gmail.com>
2025-02-10 13:14:18 -0300

    feat: add fuel, renavam and chassis field

--- server/src/tests/fixtures/car.js
+++ server/src/tests/fixtures/car.js
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker';
+import { faker, fakerPT_BR } from '@faker-js/faker';
 
 export const carFixture = {
     id: faker.string.uuid(),
@@ -7,6 +7,9 @@ export const carFixture = {
     name: faker.vehicle.model(),
     year: 2018,
     plate: 'AAA0A00',
+    fuel: fakerPT_BR.vehicle.fuel(),
+    renavam: faker.number.int({ min: 10000000000, max: 90000000000 }).toString(),
+    chassis: fakerPT_BR.vehicle.vin(),
     km: faker.number.int({ min: 0, max: 200000 }),
     entry_price: Number(faker.commerce.price({ min: 20000, max: 1000000 })),
 };
