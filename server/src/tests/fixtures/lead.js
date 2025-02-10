8991f1120bbca6187df6fae575cda64c99b911a5
Eduardo Barreira <edubarreira1212@gmail.com>
2025-02-10 13:14:33 -0300

    feat: add cpfCnpj field

--- server/src/tests/fixtures/lead.js
+++ server/src/tests/fixtures/lead.js
@@ -5,6 +5,7 @@ export const leadFixture = {
     user_id: faker.string.uuid(),
     name: faker.person.firstName(),
     email: faker.internet.email(),
+    cpfCnpj: '000.000.000-00',
     phone: fakerPT_BR.phone.number({ style: 'national' }),
     birthday: faker.date.birthdate(),
 };
